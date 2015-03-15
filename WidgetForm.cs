e85d0d7bda1f31023d5b4c548a157a7142b73237
clown <tt.clown@gmail.com>
2015-03-15 17:07:51 +0900

    WidgetForm クラスを追加

---
+++ WidgetForm.cs
@@ -0,0 +1,152 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// WidgetForm.cs
+/// 
+/// Copyright (c) 2010 CubeSoft, Inc.
+/// 
+/// This is distributed under the Microsoft Public License (Ms-PL).
+/// See http://www.opensource.org/licenses/ms-pl.html
+///
+/* ------------------------------------------------------------------------- */
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Cube.Forms
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Cube.Forms.WidgetForm
+    /// 
+    /// <summary>
+    /// Widget アプリケーション用のフォームを作成するためのクラスです。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public class WidgetForm : Form
+    {
+        #region Constructors
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// WidgetForm
+        ///
+        /// <summary>
+        /// オブジェクトを初期化します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public WidgetForm()
+            : base()
+        {
+            AutoScaleMode = AutoScaleMode.None;
+            DoubleBuffered = true;
+            Font = new Font(Font.Name, 12, Font.Style, GraphicsUnit.Pixel);
+            FormBorderStyle = FormBorderStyle.None;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            SizeGripStyle = SizeGripStyle.Hide;
+        }
+
+        #endregion
+
+        #region Hiding properties
+
+        [Browsable(false)]
+        public new AutoScaleMode AutoScaleMode
+        {
+            get { return base.AutoScaleMode; }
+            set { base.AutoScaleMode = value; }
+        }
+
+        #endregion
+
+        #region Override properties and methods
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// CreateParams
+        /// 
+        /// <summary>
+        /// コントロールの作成時に必要な情報をカプセル化します。
+        /// WidgetForm クラスでは、フォームに陰影を付与するための
+        /// パラメータをベースの値に追加しています。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                var cp = base.CreateParams;
+                cp.ClassStyle |= 0x00020000;
+                return cp;
+            }
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// OnMouseDown
+        /// 
+        /// <summary>
+        /// マウスクリック時に発生するイベントです。
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// ドラッグ中のマウス移動にフォームが追随するようにカスタマイズ
+        /// します。
+        /// </remarks>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Win32Api.ReleaseCapture();
+                Win32Api.SendMessage(Handle, Win32Api.WM_NCLBUTTONDOWN,
+                    (IntPtr)Win32Api.HT_CAPTION, IntPtr.Zero);
+            }
+            base.OnMouseDown(e);
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// OnControlAdded
+        /// 
+        /// <summary>
+        /// コントロールが追加された時に発生するイベントです。
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// 追加されたコントロールに対しても、ドラッグ中のマウス移動に
+        /// フォームが追随するようにイベントハンドラを設定します。
+        /// </remarks>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected override void OnControlAdded(ControlEventArgs e)
+        {
+            e.Control.MouseDown += (s, ev) => OnMouseDown(ev);
+            base.OnControlAdded(e);
+        }
+
+        #endregion
+
+        #region Win32 APIs
+
+        internal class Win32Api
+        {
+            public const int WM_NCLBUTTONDOWN = 0xA1;
+            public const int HT_CAPTION = 0x2;
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            public static extern IntPtr SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            public static extern bool ReleaseCapture();
+        }
+
+        #endregion
+    }
+}
