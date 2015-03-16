7aab235a3ceb88d3783aa14ae06768fe0268c5a5
clown <tt.clown@gmail.com>
2015-03-16 13:14:16 +0900

    テストを更新

--- Tests/SettingsTester.cs
+++ Tests/SettingsTester.cs
@@ -39,7 +39,7 @@ namespace Cube.Tests
 
         /* ----------------------------------------------------------------- */
         ///
-        /// TestLoadFile
+        /// TestLoad
         /// 
         /// <summary>
         /// ファイルから設定を読み込むテストを行います。
@@ -50,7 +50,7 @@ namespace Cube.Tests
         [TestCase(Cube.Settings.FileType.Json, "Settings.json", "Mike Davis")]
         [TestCase(Cube.Settings.FileType.Xml,  "SettingsJapanese.xml",  "鈴木一朗")]
         [TestCase(Cube.Settings.FileType.Json, "SettingsJapanese.json", "山田太郎")]
-        public void TestLoadFile(Cube.Settings.FileType type, string filename, string expected)
+        public void TestLoad(Cube.Settings.FileType type, string filename, string expected)
         {
             Assert.DoesNotThrow(() => {
                 var path = System.IO.Path.Combine(Examples, filename);
@@ -77,11 +77,85 @@ namespace Cube.Tests
                 {
                     var data = Cube.Settings.Load<Person>(registrar.TargetKey);
                     Assert.That(data, Is.Not.Null);
+                    Assert.That(data.Activation, Is.True);
                     Assert.That(data.Name, Is.EqualTo("Harry Potter"));
                     Assert.That(data.Age, Is.EqualTo(11));
+                    Assert.That(data.Creation.Year, Is.EqualTo(2015));
+                    Assert.That(data.Creation.Month, Is.EqualTo(3));
+                    Assert.That(data.Creation.Day, Is.EqualTo(16));
                     Assert.That(data.Phone.Type, Is.EqualTo("Mobile"));
                     Assert.That(data.Phone.Number, Is.EqualTo("090-1234-5678"));
                     Assert.That(data.Secret, Is.Null);
+                }
+            });
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// TestLoadRegistryAlias
+        /// 
+        /// <summary>
+        /// レジストリから別名が設定されている値を読み込むテストを行います。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public void TestLoadRegistryAlias()
+        {
+            Assert.DoesNotThrow(() => {
+                using (var registrar = new Registrar())
+                {
+                    var data = Cube.Settings.Load<Person>(registrar.TargetKey);
+                    Assert.That(data, Is.Not.Null);
+                    Assert.That(data.Identification, Is.EqualTo(1357));
+                }
+            });
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// TestLoadRegistryBool
+        /// 
+        /// <summary>
+        /// レジストリから真偽値を読み込むテストを行います。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [Test]
+        public void TestLoadRetistryBool()
+        {
+            Assert.DoesNotThrow(() => {
+                using (var registrar = new Registrar())
+                {
+                    var data = Cube.Settings.Load<Person>(registrar.TargetKey);
+                    Assert.That(data, Is.Not.Null);
+                    Assert.That(data.Activation, Is.True);
+                }
+            });
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// TestLoadRegistryDateTime
+        /// 
+        /// <summary>
+        /// レジストリから時刻オブジェクトを読み込むテストを行います。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [Test]
+        public void TestLoadRegistryDateTime()
+        {
+            Assert.DoesNotThrow(() => {
+                using (var registrar = new Registrar())
+                {
+                    var data = Cube.Settings.Load<Person>(registrar.TargetKey);
+                    Assert.That(data, Is.Not.Null);
+                    Assert.That(data.Creation.Year,   Is.EqualTo(2015));
+                    Assert.That(data.Creation.Month,  Is.EqualTo(3));
+                    Assert.That(data.Creation.Day,    Is.EqualTo(16));
+                    Assert.That(data.Creation.Hour,   Is.EqualTo(11));
+                    Assert.That(data.Creation.Minute, Is.EqualTo(32));
+                    Assert.That(data.Creation.Second, Is.EqualTo(26));
                 }
             });
         }
@@ -100,6 +174,9 @@ namespace Cube.Tests
         [DataContract]
         internal class Person
         {
+            [DataMember(Name = "ID")]
+            public int Identification { get; set; }
+
             [DataMember]
             public string Name { get; set; }
 
@@ -193,12 +270,17 @@ namespace Cube.Tests
                 var name = @"Software\CubeSoft";
                 _parent = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(name);
                 _target = _parent.CreateSubKey(_root);
+                _target.SetValue("ID", 1357);
+                _target.SetValue("Activation", true);
                 _target.SetValue("Name", "Harry Potter");
                 _target.SetValue("Age", 11);
+                _target.SetValue("Creation", 0x550640ba);
 
-                _child = _target.CreateSubKey(_subkey);
-                _child.SetValue("Type", "Mobile");
-                _child.SetValue("Number", "090-1234-5678");
+                using (var child = _target.CreateSubKey("Phone"))
+                {
+                    child.SetValue("Type", "Mobile");
+                    child.SetValue("Number", "090-1234-5678");
+                }
             }
 
             private void Delete()
@@ -214,9 +296,7 @@ namespace Cube.Tests
 
             private Microsoft.Win32.RegistryKey _parent = null;
             private Microsoft.Win32.RegistryKey _target = null;
-            private Microsoft.Win32.RegistryKey _child = null;
             private string _root = "SettingsTester";
-            private string _subkey = "Phone";
             private bool _disposed = false;
 
             #endregion
