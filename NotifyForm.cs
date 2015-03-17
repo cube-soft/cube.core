97c961642527c888dde342b25ba8e65f0a286fd8
clown <tt.clown@gmail.com>
2015-03-17 18:04:17 +0900

    基本的な外観を作成

---
+++ NotifyForm.cs
@@ -0,0 +1,45 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// NotifyForm.cs
+/// 
+/// Copyright (c) 2010 CubeSoft, Inc.
+/// 
+/// This is distributed under the Microsoft Public License (Ms-PL).
+/// See http://www.opensource.org/licenses/ms-pl.html
+///
+/* ------------------------------------------------------------------------- */
+using System;
+
+namespace Cube.Forms
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Cube.Forms.NotifyForm
+    /// 
+    /// <summary>
+    /// 通知用フォームを作成するためのクラスです。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public partial class NotifyForm : WidgetForm
+    {
+        #region Constructors
+
+        /* --------------------------------------------------------------------- */
+        ///
+        /// NotifyForm
+        /// 
+        /// <summary>
+        /// オブジェクトを初期化します。
+        /// </summary>
+        ///
+        /* --------------------------------------------------------------------- */
+        public NotifyForm()
+        {
+            InitializeComponent();
+            CloseButton.Click += (s, e) => Close();
+        }
+
+        #endregion
+    }
+}
