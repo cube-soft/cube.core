3c642b55d9bbd35afc23d037ce30f8964fe1214d
clown <tt.clown@gmail.com>
2015-03-21 01:22:54 +0900

    PropertyEventArgs を追加

---
+++ PropertyEventArgs.cs
@@ -0,0 +1,73 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// PropertyEventArgs.cs
+/// 
+/// Copyright (c) 2010 CubeSoft, Inc.
+/// 
+/// This is distributed under the Microsoft Public License (Ms-PL).
+/// See http://www.opensource.org/licenses/ms-pl.html
+///
+/* ------------------------------------------------------------------------- */
+using System;
+
+namespace Cube
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Cube.PropertyEventArgs
+    /// 
+    /// <summary>
+    /// プロパティに関連するイベントが発生した時の情報を保持するための
+    /// クラスです。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public class PropertyEventArgs : EventArgs
+    {
+        #region Constructors
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// PropertyEventArgs
+        /// 
+        /// <summary>
+        /// オブジェクトを初期化します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public PropertyEventArgs(string name, object value)
+            : base()
+        {
+            Name = name;
+            Value = value;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Name
+        /// 
+        /// <summary>
+        /// プロパティの名前を取得します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public string Name { get; private set; }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Value
+        /// 
+        /// <summary>
+        /// プロパティの値を取得します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public object Value { get; private set; }
+
+        #endregion
+    }
+}
