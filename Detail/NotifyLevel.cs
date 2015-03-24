0d4ecb63236584313763d5503d0c9fc66d0c254b
clown <tt.clown@gmail.com>
2015-03-24 18:49:38 +0900

    通知機能に関するクラスを追加

---
+++ Detail/NotifyLevel.cs
@@ -0,0 +1,33 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// NotifyLevel.cs
+/// 
+/// Copyright (c) 2010 CubeSoft, Inc.
+/// 
+/// This is distributed under the Microsoft Public License (Ms-PL).
+/// See http://www.opensource.org/licenses/ms-pl.html
+///
+/* ------------------------------------------------------------------------- */
+using System;
+
+namespace Cube.Forms
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Cube.Forms.NotifyLevel
+    /// 
+    /// <summary>
+    /// 通知した項目の重要度を示す値を定義した列挙体です。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public enum NotifyLevel : int
+    {
+        None        = 0,
+        Information = 1,
+        Recommended = 2,
+        Important   = 3,
+        Warning     = 4,
+        Error       = 5,
+    }
+}
