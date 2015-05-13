51f9bf377e5d0e439ae29fcf9c9a355cf7fc6530
clown <tt.clown@gmail.com>
2015-05-13 18:42:03 +0900

    アイコン生成に関連する処理を Cube.Core に移譲

--- Demo/DemoStockIcons.cs
+++ Demo/DemoStockIcons.cs
@@ -50,13 +50,12 @@ namespace Cube.Forms.Demo
             InitializeComponent();
 
             IconListView.LargeImageList = new ImageList();
-            IconListView.LargeImageList.ImageSize = new Size(48, 48);
+            IconListView.LargeImageList.ImageSize = new Size(32, 32);
             IconListView.LargeImageList.ColorDepth = ColorDepth.Depth32Bit;
-            foreach (StockIcons.Kind kind in Enum.GetValues(typeof(StockIcons.Kind)))
+            foreach (Cube.StockIcons kind in Enum.GetValues(typeof(Cube.StockIcons)))
             {
-                // var icon = Cube.Forms.StockIcons.Create(kind, StockIcons.Size.ExtraLarge);
-                var icon = Cube.Forms.StockIcons.Create(kind, StockIcons.Size.Large);
-                var text = string.Format("{0} ({1}x{2})\n{3}", (int)kind, icon.Size.Width, icon.Size.Height, kind);
+                var icon = Cube.IconFactory.Create(kind, Cube.IconSize.Large);
+                var text = string.Format("{0}\n{1}", (int)kind, kind);
                 IconListView.LargeImageList.Images.Add(icon.ToBitmap());
                 IconListView.Items.Add(new ListViewItem(text, IconListView.LargeImageList.Images.Count - 1));
             }
