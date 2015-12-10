69cc1894a1421e328db89c3277f8df5d75a3b87d
tanikado <akito1131@gmail.com>
2015-12-10 20:47:47 +0900

    TypeName が取得できないバグを修正

--- NativeApis/SHFileInfo.cs
+++ NativeApis/SHFileInfo.cs
@@ -36,7 +36,7 @@ namespace Cube
         public IntPtr hIcon;
         public int iIcon;
         public uint dwAttributes;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
         public string szDisplayName;
         [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
         public string szTypeName;
