97fce32a8453c37ecafee950984ea468bec1ed7b
clown <tt.clown@gmail.com>
2016-02-07 16:18:56 +0900

    SettingsForm を追加

---
+++ Libraries/SettingsForm.cs
@@ -0,0 +1,288 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// SettingsForm.cs
+/// 
+/// Copyright (c) 2010 CubeSoft, Inc.
+/// 
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///  http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/* ------------------------------------------------------------------------- */
+using System;
+using System.ComponentModel;
+
+namespace Cube.Forms
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// SettingsForm
+    /// 
+    /// <summary>
+    /// 設定フォーム用のクラスです。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public class SettingsForm : Form
+    {
+        #region Contructors
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// SettingsForm
+        ///
+        /// <summary>
+        /// オブジェクトを初期化します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public SettingsForm() : base() { }
+
+        #endregion
+
+        #region Properties
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// ApplyButton
+        ///
+        /// <summary>
+        /// 適用ボタンを表すコントロールを取得または設定します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [Browsable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public System.Windows.Forms.Control ApplyButton
+        {
+            get { return _apply; }
+            set
+            {
+                if (_apply == value) return;
+                if (_apply != null) _apply.Click -= ApplyButton_Click;
+                _apply = value;
+                if (_apply != null)
+                {
+                    _apply.Click -= ApplyButton_Click;
+                    _apply.Click += ApplyButton_Click;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Applied
+        ///
+        /// <summary>
+        /// 適用ボタンが押下された時に発生するイベントです。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public event EventHandler Applied;
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// PropertyChanged
+        ///
+        /// <summary>
+        /// 各種コントロールの内容が変化した時に発生するイベントです。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public event EventHandler<KeyValueEventArgs<string, object>> PropertyChanged;
+
+        #endregion
+
+        #region Virtual methods
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// OnApplied
+        ///
+        /// <summary>
+        /// Applied イベントを発生させます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected virtual void OnApplied(EventArgs e)
+        {
+            if (Applied != null) Applied(this, e);
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// OnPropertyChanged
+        ///
+        /// <summary>
+        /// PropertyChanged イベントを発生させます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected virtual void OnPropertyChanged(KeyValueEventArgs<string, object> e)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, e);
+            if (ApplyButton != null) ApplyButton.Enabled = true;
+        }
+
+        #endregion
+
+        #region Event handlers
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// ApplyButton_Click
+        ///
+        /// <summary>
+        /// 適用ボタンが押下された時に実行されるハンドラです。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private void ApplyButton_Click(object sender, EventArgs e)
+        {
+            if (ApplyButton == null) return;
+            OnApplied(e);
+            ApplyButton.Enabled = false;
+        }
+
+        #endregion
+
+        #region Others
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// CheckBoxChanged
+        ///
+        /// <summary>
+        /// チェックボックスの状態が変化した時に実行されるハンドラです。
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// Value には CheckBox.Checked が設定されます。
+        /// </remarks>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected void CheckBoxChanged(object sender, EventArgs e)
+        {
+            var control = sender as System.Windows.Forms.CheckBox;
+            if (control == null) return;
+            RaisePropertyChanged(control, control.Checked);
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// ComboBoxChanged
+        ///
+        /// <summary>
+        /// コンボボックスの状態が変化した時に実行されるハンドラです。
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// Value には ComboBox.SelectedIndex が設定されます。
+        /// </remarks>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected void ComboBoxChanged(object sender, EventArgs e)
+        {
+            var control = sender as System.Windows.Forms.ComboBox;
+            if (control == null) return;
+            RaisePropertyChanged(control, control.SelectedIndex);
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// NumericUpDownChanged
+        ///
+        /// <summary>
+        /// NumericUpDown の状態が変化した時に実行されるハンドラです。
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// Value には NumericUpDown.Value が設定されます。
+        /// </remarks>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected void NumericUpDownChanged(object sender, EventArgs e)
+        {
+            var control = sender as System.Windows.Forms.NumericUpDown;
+            if (control == null) return;
+            RaisePropertyChanged(control, control.Value);
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// RadioButtonChanged
+        ///
+        /// <summary>
+        /// ラジオボタンの状態が変化した時に実行されるハンドラです。
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// Value には RadioButton.Checked が設定されます。
+        /// </remarks>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected void RadioButtonChanged(object sender, EventArgs e)
+        {
+            var control = sender as System.Windows.Forms.RadioButton;
+            if (control == null) return;
+            RaisePropertyChanged(control, control.Checked);
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// TextBoxChanged
+        ///
+        /// <summary>
+        /// ラジオボタンの状態が変化した時に実行されるハンドラです。
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// Value には TextBox.Text が設定されます。
+        /// </remarks>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected void TextBoxChanged(object sender, EventArgs e)
+        {
+            var control = sender as System.Windows.Forms.TextBox;
+            if (control == null) return;
+            RaisePropertyChanged(control, control.Text);
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// RaisePropertyChanged
+        ///
+        /// <summary>
+        /// PropertyChanged イベントを発生させます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected void RaisePropertyChanged(System.Windows.Forms.Control control, object value)
+        {
+            var name = control.Tag is string ?
+                       control.Tag as string :
+                       control.Name.Replace(control.GetType().Name, string.Empty);
+            if (string.IsNullOrEmpty(name)) return;
+
+            OnPropertyChanged(new KeyValueEventArgs<string, object>(name, value));
+        }
+
+        #endregion
+
+        #region Fields
+        private System.Windows.Forms.Control _apply;
+        #endregion
+    }
+}
