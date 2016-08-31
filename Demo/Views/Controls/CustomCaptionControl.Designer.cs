150f47ace030965261970379833971d130cdf056
clown <tt.clown@gmail.com>
2016-08-31 21:55:55 +0900

    CustomCaptionControl を追加

---
+++ Demo/Views/Controls/CustomCaptionControl.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace Cube.Forms.Demo
+{
+    partial class CustomCaptionControl
+    {
+        /// <summary> 
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary> 
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Cube.Forms.ButtonStyle buttonStyle11 = new Cube.Forms.ButtonStyle();
+            Cube.Forms.ButtonStyle buttonStyle12 = new Cube.Forms.ButtonStyle();
+            Cube.Forms.ButtonStyle buttonStyle13 = new Cube.Forms.ButtonStyle();
+            Cube.Forms.ButtonStyle buttonStyle14 = new Cube.Forms.ButtonStyle();
+            Cube.Forms.ButtonStyle buttonStyle15 = new Cube.Forms.ButtonStyle();
+            Cube.Forms.ButtonStyle buttonStyle6 = new Cube.Forms.ButtonStyle();
+            Cube.Forms.ButtonStyle buttonStyle7 = new Cube.Forms.ButtonStyle();
+            Cube.Forms.ButtonStyle buttonStyle8 = new Cube.Forms.ButtonStyle();
+            Cube.Forms.ButtonStyle buttonStyle9 = new Cube.Forms.ButtonStyle();
+            Cube.Forms.ButtonStyle buttonStyle10 = new Cube.Forms.ButtonStyle();
+            Cube.Forms.ButtonStyle buttonStyle1 = new Cube.Forms.ButtonStyle();
+            Cube.Forms.ButtonStyle buttonStyle2 = new Cube.Forms.ButtonStyle();
+            Cube.Forms.ButtonStyle buttonStyle3 = new Cube.Forms.ButtonStyle();
+            Cube.Forms.ButtonStyle buttonStyle4 = new Cube.Forms.ButtonStyle();
+            Cube.Forms.ButtonStyle buttonStyle5 = new Cube.Forms.ButtonStyle();
+            this.LayoutPanel = new Cube.Forms.TableLayoutPanel();
+            this.LogoPictureBox = new Cube.Forms.PictureBox();
+            this.TitlePictureBox = new Cube.Forms.PictureBox();
+            this.PaddingPanel = new Cube.Forms.Panel();
+            this.MinimizeButton = new Cube.Forms.FlatButton();
+            this.MaximizeButton = new Cube.Forms.FlatButton();
+            this.ExitButton = new Cube.Forms.FlatButton();
+            this.LayoutPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TitlePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LayoutPanel
+            // 
+            this.LayoutPanel.ColumnCount = 6;
+            this.LayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.LayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.LayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.LayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.LayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.LayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.LayoutPanel.Controls.Add(this.ExitButton, 5, 0);
+            this.LayoutPanel.Controls.Add(this.MaximizeButton, 4, 0);
+            this.LayoutPanel.Controls.Add(this.LogoPictureBox, 0, 0);
+            this.LayoutPanel.Controls.Add(this.TitlePictureBox, 1, 0);
+            this.LayoutPanel.Controls.Add(this.PaddingPanel, 2, 0);
+            this.LayoutPanel.Controls.Add(this.MinimizeButton, 3, 0);
+            this.LayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.LayoutPanel.Name = "LayoutPanel";
+            this.LayoutPanel.RowCount = 1;
+            this.LayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.LayoutPanel.Size = new System.Drawing.Size(400, 30);
+            this.LayoutPanel.TabIndex = 0;
+            // 
+            // LogoPictureBox
+            // 
+            this.LogoPictureBox.Image = global::Cube.Forms.Demo.Properties.Resources.CubeSmall;
+            this.LogoPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.LogoPictureBox.Margin = new System.Windows.Forms.Padding(0);
+            this.LogoPictureBox.Name = "LogoPictureBox";
+            this.LogoPictureBox.Size = new System.Drawing.Size(30, 30);
+            this.LogoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.LogoPictureBox.TabIndex = 0;
+            this.LogoPictureBox.TabStop = false;
+            // 
+            // TitlePictureBox
+            // 
+            this.TitlePictureBox.Image = global::Cube.Forms.Demo.Properties.Resources.Title;
+            this.TitlePictureBox.Location = new System.Drawing.Point(30, 0);
+            this.TitlePictureBox.Margin = new System.Windows.Forms.Padding(0);
+            this.TitlePictureBox.Name = "TitlePictureBox";
+            this.TitlePictureBox.Size = new System.Drawing.Size(50, 30);
+            this.TitlePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.TitlePictureBox.TabIndex = 1;
+            this.TitlePictureBox.TabStop = false;
+            // 
+            // PaddingPanel
+            // 
+            this.PaddingPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PaddingPanel.Location = new System.Drawing.Point(80, 0);
+            this.PaddingPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.PaddingPanel.Name = "PaddingPanel";
+            this.PaddingPanel.Size = new System.Drawing.Size(230, 30);
+            this.PaddingPanel.TabIndex = 2;
+            // 
+            // MinimizeButton
+            // 
+            this.MinimizeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.MinimizeButton.Content = "";
+            this.MinimizeButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MinimizeButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.MinimizeButton.FlatAppearance.BorderSize = 0;
+            this.MinimizeButton.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.MinimizeButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.MinimizeButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.MinimizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MinimizeButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.MinimizeButton.Image = null;
+            this.MinimizeButton.Location = new System.Drawing.Point(310, 0);
+            this.MinimizeButton.Margin = new System.Windows.Forms.Padding(0);
+            this.MinimizeButton.Name = "MinimizeButton";
+            this.MinimizeButton.Size = new System.Drawing.Size(30, 30);
+            buttonStyle11.BackColor = System.Drawing.Color.Empty;
+            buttonStyle11.BackgroundImage = null;
+            buttonStyle11.BorderColor = System.Drawing.Color.Empty;
+            buttonStyle11.ContentColor = System.Drawing.Color.Empty;
+            buttonStyle11.Image = null;
+            this.MinimizeButton.Styles.Checked = buttonStyle11;
+            buttonStyle12.BackColor = System.Drawing.Color.Empty;
+            buttonStyle12.BackgroundImage = null;
+            buttonStyle12.BorderColor = System.Drawing.Color.Empty;
+            buttonStyle12.ContentColor = System.Drawing.Color.Empty;
+            buttonStyle12.Image = null;
+            this.MinimizeButton.Styles.Disabled = buttonStyle12;
+            buttonStyle13.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            buttonStyle13.BackgroundImage = null;
+            buttonStyle13.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            buttonStyle13.BorderSize = 1;
+            buttonStyle13.ContentColor = System.Drawing.Color.Empty;
+            buttonStyle13.Image = null;
+            this.MinimizeButton.Styles.MouseDown = buttonStyle13;
+            buttonStyle14.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            buttonStyle14.BackgroundImage = null;
+            buttonStyle14.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(228)))), ((int)(((byte)(228)))));
+            buttonStyle14.BorderSize = 1;
+            buttonStyle14.ContentColor = System.Drawing.Color.Empty;
+            buttonStyle14.Image = null;
+            this.MinimizeButton.Styles.MouseOver = buttonStyle14;
+            buttonStyle15.BackColor = System.Drawing.Color.Empty;
+            buttonStyle15.BackgroundImage = null;
+            buttonStyle15.BorderColor = System.Drawing.Color.Empty;
+            buttonStyle15.ContentColor = System.Drawing.SystemColors.WindowText;
+            buttonStyle15.Image = global::Cube.Forms.Demo.Properties.Resources.Minimize;
+            this.MinimizeButton.Styles.Normal = buttonStyle15;
+            this.MinimizeButton.TabIndex = 3;
+            this.MinimizeButton.UseVisualStyleBackColor = false;
+            // 
+            // MaximizeButton
+            // 
+            this.MaximizeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.MaximizeButton.Content = "";
+            this.MaximizeButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MaximizeButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.MaximizeButton.FlatAppearance.BorderSize = 0;
+            this.MaximizeButton.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.MaximizeButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.MaximizeButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.MaximizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MaximizeButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.MaximizeButton.Image = null;
+            this.MaximizeButton.Location = new System.Drawing.Point(340, 0);
+            this.MaximizeButton.Margin = new System.Windows.Forms.Padding(0);
+            this.MaximizeButton.Name = "MaximizeButton";
+            this.MaximizeButton.Size = new System.Drawing.Size(30, 30);
+            buttonStyle6.BackColor = System.Drawing.Color.Empty;
+            buttonStyle6.BackgroundImage = null;
+            buttonStyle6.BorderColor = System.Drawing.Color.Empty;
+            buttonStyle6.ContentColor = System.Drawing.Color.Empty;
+            buttonStyle6.Image = null;
+            this.MaximizeButton.Styles.Checked = buttonStyle6;
+            buttonStyle7.BackColor = System.Drawing.Color.Empty;
+            buttonStyle7.BackgroundImage = null;
+            buttonStyle7.BorderColor = System.Drawing.Color.Empty;
+            buttonStyle7.ContentColor = System.Drawing.Color.Empty;
+            buttonStyle7.Image = null;
+            this.MaximizeButton.Styles.Disabled = buttonStyle7;
+            buttonStyle8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            buttonStyle8.BackgroundImage = null;
+            buttonStyle8.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            buttonStyle8.BorderSize = 1;
+            buttonStyle8.ContentColor = System.Drawing.Color.Empty;
+            buttonStyle8.Image = null;
+            this.MaximizeButton.Styles.MouseDown = buttonStyle8;
+            buttonStyle9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            buttonStyle9.BackgroundImage = null;
+            buttonStyle9.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(228)))), ((int)(((byte)(228)))));
+            buttonStyle9.BorderSize = 1;
+            buttonStyle9.ContentColor = System.Drawing.Color.Empty;
+            buttonStyle9.Image = null;
+            this.MaximizeButton.Styles.MouseOver = buttonStyle9;
+            buttonStyle10.BackColor = System.Drawing.Color.Empty;
+            buttonStyle10.BackgroundImage = null;
+            buttonStyle10.BorderColor = System.Drawing.Color.Empty;
+            buttonStyle10.ContentColor = System.Drawing.SystemColors.WindowText;
+            buttonStyle10.Image = global::Cube.Forms.Demo.Properties.Resources.Maximize;
+            this.MaximizeButton.Styles.Normal = buttonStyle10;
+            this.MaximizeButton.TabIndex = 4;
+            this.MaximizeButton.UseVisualStyleBackColor = false;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ExitButton.Content = "";
+            this.ExitButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ExitButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ExitButton.FlatAppearance.BorderSize = 0;
+            this.ExitButton.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ExitButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ExitButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ExitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExitButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ExitButton.Image = null;
+            this.ExitButton.Location = new System.Drawing.Point(370, 0);
+            this.ExitButton.Margin = new System.Windows.Forms.Padding(0);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(30, 30);
+            buttonStyle1.BackColor = System.Drawing.Color.Empty;
+            buttonStyle1.BackgroundImage = null;
+            buttonStyle1.BorderColor = System.Drawing.Color.Empty;
+            buttonStyle1.ContentColor = System.Drawing.Color.Empty;
+            buttonStyle1.Image = null;
+            this.ExitButton.Styles.Checked = buttonStyle1;
+            buttonStyle2.BackColor = System.Drawing.Color.Empty;
+            buttonStyle2.BackgroundImage = null;
+            buttonStyle2.BorderColor = System.Drawing.Color.Empty;
+            buttonStyle2.ContentColor = System.Drawing.Color.Empty;
+            buttonStyle2.Image = null;
+            this.ExitButton.Styles.Disabled = buttonStyle2;
+            buttonStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            buttonStyle3.BackgroundImage = null;
+            buttonStyle3.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            buttonStyle3.BorderSize = 1;
+            buttonStyle3.ContentColor = System.Drawing.Color.Empty;
+            buttonStyle3.Image = null;
+            this.ExitButton.Styles.MouseDown = buttonStyle3;
+            buttonStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            buttonStyle4.BackgroundImage = null;
+            buttonStyle4.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(228)))), ((int)(((byte)(228)))));
+            buttonStyle4.BorderSize = 1;
+            buttonStyle4.ContentColor = System.Drawing.Color.Empty;
+            buttonStyle4.Image = null;
+            this.ExitButton.Styles.MouseOver = buttonStyle4;
+            buttonStyle5.BackColor = System.Drawing.Color.Empty;
+            buttonStyle5.BackgroundImage = null;
+            buttonStyle5.BorderColor = System.Drawing.Color.Empty;
+            buttonStyle5.ContentColor = System.Drawing.SystemColors.WindowText;
+            buttonStyle5.Image = global::Cube.Forms.Demo.Properties.Resources.Close;
+            this.ExitButton.Styles.Normal = buttonStyle5;
+            this.ExitButton.TabIndex = 5;
+            this.ExitButton.UseVisualStyleBackColor = false;
+            // 
+            // CustomCaptionControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.LayoutPanel);
+            this.Name = "CustomCaptionControl";
+            this.Size = new System.Drawing.Size(400, 30);
+            this.LayoutPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TitlePictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TableLayoutPanel LayoutPanel;
+        private PictureBox LogoPictureBox;
+        private PictureBox TitlePictureBox;
+        private Panel PaddingPanel;
+        private FlatButton ExitButton;
+        private FlatButton MaximizeButton;
+        private FlatButton MinimizeButton;
+    }
+}
