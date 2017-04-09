d0ef20500d3b90f187ab5c125abf806237a4e3b1
clown <tt.clown@gmail.com>
2017-04-09 16:54:19 +0900

    Update tests

--- Tests/MessengerTest.cs
+++ Tests/MessengerTest.cs
@@ -15,6 +15,7 @@
 /// limitations under the License.
 ///
 /* ------------------------------------------------------------------------- */
+using System;
 using NUnit.Framework;
 
 namespace Cube.Tests
@@ -28,7 +29,6 @@ namespace Cube.Tests
     /// </summary>
     ///
     /* --------------------------------------------------------------------- */
-    //[Parallelizable]
     [TestFixture]
     class MessengerTest
     {
@@ -36,30 +36,82 @@ namespace Cube.Tests
 
         /* ----------------------------------------------------------------- */
         ///
-        /// Send
+        /// Publish_Server
         /// 
         /// <summary>
         /// サーバにメッセージを送信するテストを実行します。
         /// </summary>
         ///
         /* ----------------------------------------------------------------- */
-        [TestCase("Hello, world!")]
-        public void Send(string message)
+        [Test]
+        public void Publish_Server()
         {
-            var id = nameof(MessengerTest);
+            var id     = nameof(MessengerTest);
+            var msg    = "ClientToServer";
             var result = string.Empty;
 
             using (var server = new Cube.Processes.MessengerServer<string>(id))
+            using (var client = new Cube.Processes.MessengerClient<string>(id))
             {
                 server.Subscribe(x => result = x);
-                using (var client = new Cube.Processes.MessengerClient<string>(id))
-                {
-                    client.Send(message);
-                }
+                client.Publish(msg);
             }
 
-            Assert.That(result, Is.EqualTo(message));
+            Assert.That(result, Is.EqualTo(msg));
         }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Publish_Client
+        /// 
+        /// <summary>
+        /// クライアントにメッセージを送信するテストを実行します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [Test]
+        public void Publish_Client()
+        {
+            var id     = nameof(MessengerTest);
+            var msg    = "ServerToClient";
+            var result = string.Empty;
+
+            using (var server = new Cube.Processes.MessengerServer<string>(id))
+            using (var client = new Cube.Processes.MessengerClient<string>(id))
+            {
+                client.Subscribe(x => result = x);
+                server.Publish(msg);
+            }
+
+            Assert.That(result, Is.EqualTo(msg));
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Create_DuplicateServer_Throws
+        /// 
+        /// <summary>
+        /// サーバを 2 つ生成しようとするテストを実行します。
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// Messenger(T) は同一スレッド上に存在する他のサーバを検知する
+        /// 事ができないので、2 つ目のサーバを生成しようとして例外が
+        /// 発生します。
+        /// </remarks>
+        ///
+        /* ----------------------------------------------------------------- */
+        [Test]
+        public void Create_DuplicateServer_Throws()
+            => Assert.That(() =>
+            {
+                var id = nameof(MessengerTest);
+                using (var s1 = new Cube.Processes.Messenger<string>(id))
+                using (var s2 = new Cube.Processes.Messenger<string>(id))
+                {
+                    Assert.Fail("never reached");
+                }
+            }, Throws.TypeOf<InvalidOperationException>());
 
         #endregion
     }
