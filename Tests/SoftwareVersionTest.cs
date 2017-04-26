b58b97fc3b309bcd01016027af56fdff86994a59
clown <tt.clown@gmail.com>
2017-04-26 14:48:23 +0900

    Fix ToString method

--- Tests/SoftwareVersionTest.cs
+++ Tests/SoftwareVersionTest.cs
@@ -56,7 +56,9 @@ namespace Cube.Tests
             version.Prefix = "begin-";
             version.Suffix = "-end";
 
-            Assert.That(version.ToString(), Is.EqualTo($"begin-{major}.{minor}-end ({arch})"));
+            Assert.That(version.ToString(true),  Is.EqualTo($"begin-{major}.{minor}-end ({arch})"));
+            Assert.That(version.ToString(false), Is.EqualTo($"begin-{major}.{minor}-end"));
+            Assert.That(version.ToString(),      Is.EqualTo(version.ToString(false)));
         }
 
         /* ----------------------------------------------------------------- */
