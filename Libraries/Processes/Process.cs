a4ef89cbcb4dd69925cf391ad6fd6057266c24eb
clown <tt.clown@gmail.com>
2017-05-03 20:51:24 +0900

    Fix for setting username explicitly

--- Libraries/Processes/Process.cs
+++ Libraries/Processes/Process.cs
@@ -17,6 +17,7 @@
 /* ------------------------------------------------------------------------- */
 using System;
 using System.ComponentModel;
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 
@@ -45,11 +46,31 @@ namespace Cube.Processes
         /// 
         /// <param name="program">実行プログラムのパス</param>
         /// <param name="arguments">プログラムの引数</param>
+        /// 
         /// <returns>実行に成功した <c>Process</c> オブジェクト</returns>
         ///
         /* ----------------------------------------------------------------- */
         public static System.Diagnostics.Process StartAsActiveUser(string program, string[] arguments)
-            => StartAsActiveUser(CreateCmdLine(program, arguments));
+            => StartAsActiveUser(string.Empty, program, arguments);
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// StartAsActiveUser
+        ///
+        /// <summary>
+        /// アクティブユーザ権限でプログラムを実行します。
+        /// </summary>
+        /// 
+        /// <param name="username">ユーザ名</param>
+        /// <param name="program">実行プログラムのパス</param>
+        /// <param name="arguments">プログラムの引数</param>
+        /// 
+        /// <returns>実行に成功した <c>Process</c> オブジェクト</returns>
+        ///
+        /* ----------------------------------------------------------------- */
+        public static System.Diagnostics.Process StartAsActiveUser(
+            string username, string program, string[] arguments)
+            => StartAsActiveUser(username, CreateCmdLine(program, arguments));
 
         /* ----------------------------------------------------------------- */
         ///
@@ -60,15 +81,33 @@ namespace Cube.Processes
         /// </summary>
         /// 
         /// <param name="cmdline">実行するコマンドライン</param>
+        /// 
         /// <returns>実行に成功した <c>Process</c> オブジェクト</returns>
         ///
         /* ----------------------------------------------------------------- */
         public static System.Diagnostics.Process StartAsActiveUser(string cmdline)
-            => StartAs(GetActiveSessionToken(), cmdline);
+            => StartAsActiveUser(string.Empty, cmdline);
 
         /* ----------------------------------------------------------------- */
         ///
         /// StartAsActiveUser
+        ///
+        /// <summary>
+        /// アクティブユーザ権限でプログラムを実行します。
+        /// </summary>
+        /// 
+        /// <param name="username">ユーザ名</param>
+        /// <param name="cmdline">実行するコマンドライン</param>
+        /// 
+        /// <returns>実行に成功した <c>Process</c> オブジェクト</returns>
+        ///
+        /* ----------------------------------------------------------------- */
+        public static System.Diagnostics.Process StartAsActiveUser(string username, string cmdline)
+            => StartAs(GetActiveSessionToken(username), cmdline);
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// StartAs
         ///
         /// <summary>
         /// 指定されたトークンの権限でプログラムを実行します。
@@ -77,6 +116,7 @@ namespace Cube.Processes
         /// <param name="token">実行ユーザのトークン</param>
         /// <param name="program">実行プログラムのパス</param>
         /// <param name="arguments">プログラムの引数</param>
+        /// 
         /// <returns>実行に成功した <c>Process</c> オブジェクト</returns>
         ///
         /* ----------------------------------------------------------------- */
@@ -94,6 +134,7 @@ namespace Cube.Processes
         /// 
         /// <param name="token">実行ユーザのトークン</param>
         /// <param name="cmdline">実行するコマンドライン</param>
+        /// 
         /// <returns>実行に成功した <c>Process</c> オブジェクト</returns>
         ///
         /* ----------------------------------------------------------------- */
@@ -146,12 +187,14 @@ namespace Cube.Processes
         /* ----------------------------------------------------------------- */
         private static System.Diagnostics.Process CreateProcessAsUser(string cmdline, IntPtr token, IntPtr env)
         {
-            var si = new STARTUPINFO();
-            si.cb          = (uint)Marshal.SizeOf(si);
-            si.lpDesktop   = @"WinSta0\Default";
-            si.wShowWindow = 0x05;  // SW_SHOW
-            si.dwFlags     = 0x01 | // STARTF_USESHOWWINDOW 
-                             0x40;  // STARTF_FORCEONFEEDBACK
+            var si = new STARTUPINFO
+            {
+                lpDesktop   = @"WinSta0\Default",
+                wShowWindow = 0x05,  // SW_SHOW
+                dwFlags     = 0x01 | // STARTF_USESHOWWINDOW 
+                              0x40,  // STARTF_FORCEONFEEDBACK
+            };
+            si.cb = (uint)Marshal.SizeOf(si);
 
             var sa = new SECURITY_ATTRIBUTES();
             sa.nLength = (uint)Marshal.SizeOf(sa);
@@ -193,8 +236,10 @@ namespace Cube.Processes
         /// 現在ログオン中のユーザに対応するセッション ID を取得します。
         /// </summary>
         /// 
+        /// <param name="username">ユーザ名</param>
+        /// 
         /* ----------------------------------------------------------------- */
-        private static uint GetActiveSessionId()
+        private static uint GetActiveSessionId(string username)
         {
             var ptr = IntPtr.Zero;
             var count = 0u;
@@ -209,6 +254,8 @@ namespace Cube.Processes
                     ref count
                 )) Win32Error("WTSEnumerateSessions");
 
+                var sessions = new List<WTS_SESSION_INFO>();
+
                 for (var i = 0; i < count; ++i)
                 {
                     var info = (WTS_SESSION_INFO)Marshal.PtrToStructure(
@@ -216,10 +263,12 @@ namespace Cube.Processes
                         typeof(WTS_SESSION_INFO)
                     );
 
-                    if (info.State == WTS_CONNECTSTATE_CLASS.WTSActive) return info.SessionID;
+                    if (info.State == WTS_CONNECTSTATE_CLASS.WTSActive) sessions.Add(info);
                 }
 
-                throw new ArgumentException("Active session not found");
+                if (sessions.Count <= 0) throw new ArgumentException("Active session not found");
+                else if (sessions.Count == 1) return sessions[0].SessionID;
+                else return sessions.Select(x => x.SessionID).First(x => GetUserName(x) == username);
             }
             finally { if (ptr != IntPtr.Zero) WtsApi32.NativeMethods.WTSFreeMemory(ptr); }
         }
@@ -232,10 +281,12 @@ namespace Cube.Processes
         /// アクティブなセッションに対応するトークンを取得します。
         /// </summary>
         /// 
+        /// <param name="username">ユーザ名</param>
+        /// 
         /* ----------------------------------------------------------------- */
-        private static IntPtr GetActiveSessionToken()
+        private static IntPtr GetActiveSessionToken(string username)
         {
-            var id = GetActiveSessionId();
+            var id = GetActiveSessionId(username);
             var token = IntPtr.Zero;
 
             if (!WtsApi32.NativeMethods.WTSQueryUserToken(id, out token)) Win32Error("WTSQueryUserToken");
@@ -273,6 +324,34 @@ namespace Cube.Processes
 
             if (!result) Win32Error("DuplicateTokenEx");
             return dest;
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// GetUserName
+        ///
+        /// <summary>
+        /// ユーザ名を取得します。
+        /// </summary>
+        /// 
+        /* ----------------------------------------------------------------- */
+        private static string GetUserName(uint id)
+        {
+            var handle = IntPtr.Zero;
+            var buffer = IntPtr.Zero;
+            var result = 0u;
+
+            try
+            {
+                return WtsApi32.NativeMethods.WTSQuerySessionInformation(
+                           handle,
+                           (int)id,
+                           WTS_INFO_CLASS.WTSUserName,
+                           out buffer,
+                           out result
+                       ) ? Marshal.PtrToStringAnsi(buffer) : string.Empty;
+            }
+            finally { WtsApi32.NativeMethods.WTSFreeMemory(buffer); }
         }
 
         /* ----------------------------------------------------------------- */
