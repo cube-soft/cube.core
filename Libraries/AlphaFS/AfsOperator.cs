7e5c9284317454cdf85409145cd3887bb8c4c847
clown <tt.clown@gmail.com>
2017-10-13 23:26:04 +0900

    Fix comments.

--- Libraries/AlphaFS/AfsOperator.cs
+++ Libraries/AlphaFS/AfsOperator.cs
@@ -1,19 +1,19 @@
 ﻿/* ------------------------------------------------------------------------- */
-///
-/// Copyright (c) 2010 CubeSoft, Inc.
-/// 
-/// Licensed under the Apache License, Version 2.0 (the "License");
-/// you may not use this file except in compliance with the License.
-/// You may obtain a copy of the License at
-///
-///  http://www.apache.org/licenses/LICENSE-2.0
-///
-/// Unless required by applicable law or agreed to in writing, software
-/// distributed under the License is distributed on an "AS IS" BASIS,
-/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-/// See the License for the specific language governing permissions and
-/// limitations under the License.
-///
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 /* ------------------------------------------------------------------------- */
 using System;
 using Alphaleonis.Win32.Filesystem;
@@ -22,10 +22,10 @@ namespace Cube.FileSystem
 {
     /* --------------------------------------------------------------------- */
     ///
-    /// AlphaOperatorCore
+    /// AfsOperatorCore
     /// 
     /// <summary>
-    /// AlphaFS を利用した IOperatorCore の実装クラスです。
+    /// AlphaFS を利用した Operator クラスです。
     /// </summary>
     ///
     /* --------------------------------------------------------------------- */
@@ -158,8 +158,7 @@ namespace Cube.FileSystem
         /// パスを結合します。
         /// </summary>
         /// 
-        /// <param name="directory">ディレクトリを示すパス</param>
-        /// <param name="filename">ファイル名</param>
+        /// <param name="paths">パス一覧</param>
         /// 
         /* ----------------------------------------------------------------- */
         protected override string CombineCore(params string[] paths)
--- Libraries/PathFilter.cs
+++ Libraries/PathFilter.cs
@@ -1,19 +1,19 @@
 ﻿/* ------------------------------------------------------------------------- */
-///
-/// Copyright (c) 2010 CubeSoft, Inc.
-/// 
-/// Licensed under the Apache License, Version 2.0 (the "License");
-/// you may not use this file except in compliance with the License.
-/// You may obtain a copy of the License at
-///
-///  http://www.apache.org/licenses/LICENSE-2.0
-///
-/// Unless required by applicable law or agreed to in writing, software
-/// distributed under the License is distributed on an "AS IS" BASIS,
-/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-/// See the License for the specific language governing permissions and
-/// limitations under the License.
-///
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 /* ------------------------------------------------------------------------- */
 using System.Collections.Generic;
 using System.Linq;
@@ -650,8 +650,11 @@ namespace Cube.FileSystem
         /* ----------------------------------------------------------------- */
         protected enum PathKind
         {
+            /// <summary>通常のパス</summary>
             Normal,
+            /// <summary>UNC パス</summary>
             Unc,
+            /// <summary>サービス機能の不活性化されたパス</summary>
             Inactivation,
         }
 
