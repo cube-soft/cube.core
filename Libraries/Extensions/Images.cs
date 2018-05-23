6723da3c422a5bb24fdf0b60c2b1adebbee507c8
clown <tt.clown@gmail.com>
2018-05-23 15:48:27 +0900

    Fix namespace.

--- Libraries/Extensions/Images.cs
+++ Libraries/Extensions/Images.cs
@@ -19,7 +19,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 
-namespace Cube.Forms
+namespace Cube.Forms.Images
 {
     /* --------------------------------------------------------------------- */
     ///
