2938bcb627203e795c22eb7c5fe5788449ff921c
clown <tt.clown@gmail.com>
2018-05-29 10:38:33 +0900

    Add static method for creating KeyValuePiar object.

---
+++ Libraries/Collections/KeyValue.cs
@@ -0,0 +1,54 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+/* ------------------------------------------------------------------------- */
+using System.Collections.Generic;
+
+namespace Cube
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// KeyValue
+    ///
+    /// <summary>
+    /// KeyValuePair(T, U) の拡張用クラスです。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public static class KeyValue
+    {
+        #region Methods
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Create
+        ///
+        /// <summary>
+        /// KeyValuePair(T, U) オブジェクトを生成します。
+        /// </summary>
+        ///
+        /// <param name="key">キー</param>
+        /// <param name="value">値</param>
+        ///
+        /// <returns>KeyValuePair(T, U) オブジェクト</returns>
+        ///
+        /* ----------------------------------------------------------------- */
+        public static KeyValuePair<T, U> Create<T, U>(T key, U value) =>
+            new KeyValuePair<T, U>(key, value);
+
+        #endregion
+    }
+}
