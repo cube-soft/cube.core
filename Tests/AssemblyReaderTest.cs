bf8ea6b90cc50f45d2e788d89bcde5f5ecf52137
clown <tt.clown@gmail.com>
2018-07-01 14:45:16 +0900

    Fix version number.

--- Tests/AssemblyReaderTest.cs
+++ Tests/AssemblyReaderTest.cs
@@ -86,8 +86,8 @@ namespace Cube.Tests
                     Trademark     = "CubeSoft, Inc.",
                     Configuration = string.Empty,
                     Culture       = string.Empty,
-                    Version       = new Version(1, 10, 0, 0),
-                    FileVersion   = new Version(1, 10, 0, 0)
+                    Version       = new Version(1, 11, 0, 0),
+                    FileVersion   = new Version(1, 11, 0, 0)
                 });
 
                 yield return new TestCaseData(Assembly.GetAssembly(typeof(AssemblyReader)), new Result
@@ -102,8 +102,8 @@ namespace Cube.Tests
                     Trademark     = string.Empty,
                     Configuration = string.Empty,
                     Culture       = string.Empty,
-                    Version       = new Version(1, 10, 0, 0),
-                    FileVersion   = new Version(1, 10, 0, 0)
+                    Version       = new Version(1, 11, 0, 0),
+                    FileVersion   = new Version(1, 11, 0, 0)
                 });
 
                 yield return new TestCaseData(null, new Result
