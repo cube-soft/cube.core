ab4e537771fc7f53ba28f198922b08b8cb44e683
clown <tt.clown@gmail.com>
2018-07-09 16:10:24 +0900

    Add BindableFunc.

---
+++ Libraries/Sources/BindableFunc.cs
@@ -0,0 +1,191 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+/* ------------------------------------------------------------------------- */
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Cube.Xui
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// BindableFunc(T)
+    ///
+    /// <summary>
+    /// 関数オブジェクトの結果を Binding 可能にするためのクラスです。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public class BindableFunc<T> : INotifyPropertyChanged
+    {
+        #region Constructors
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// BindableFunc
+        ///
+        /// <summary>
+        /// オブジェクトを初期化します。
+        /// </summary>
+        ///
+        /// <param name="func">関数オブジェクト</param>
+        ///
+        /* ----------------------------------------------------------------- */
+        public BindableFunc(Func<T> func) : this(func, SynchronizationContext.Current) { }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// BindableFunc
+        ///
+        /// <summary>
+        /// オブジェクトを初期化します。
+        /// </summary>
+        ///
+        /// <param name="func">関数オブジェクト</param>
+        /// <param name="context">同期用コンテキスト</param>
+        ///
+        /* ----------------------------------------------------------------- */
+        public BindableFunc(Func<T> func, SynchronizationContext context)
+        {
+            Func = func;
+            _context = context;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Value
+        ///
+        /// <summary>
+        /// 値を取得します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public T Value => Func();
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// HasValue
+        ///
+        /// <summary>
+        /// 有効な値が設定されているかどうかを示す値を取得します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public bool HasValue => Func != null;
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// IsSynchronous
+        ///
+        /// <summary>
+        /// UI スレッドに対して同期的にイベントを発生させるかどうかを
+        /// 示す値を取得または設定します。
+        /// </summary>
+        ///
+        /// <remarks>
+        /// true の場合は Send メソッド、false の場合は Post メソッドを
+        /// 用いてイベントを伝搬します。
+        /// </remarks>
+        ///
+        /* ----------------------------------------------------------------- */
+        public bool IsSynchronous { get; set; } = true;
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Func
+        ///
+        /// <summary>
+        /// Value の内容を決定するための関数オブジェクトを取得します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected Func<T> Func { get; }
+
+        #endregion
+
+        #region Events
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// PropertyChanged
+        ///
+        /// <summary>
+        /// プロパティの内容変更時に発生するイベントです。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// OnPropertyChanged
+        ///
+        /// <summary>
+        /// PropertyChanged イベントを発生させます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e) =>
+            PropertyChanged?.Invoke(this, e);
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// RaisePropertyChanged
+        ///
+        /// <summary>
+        /// PropertyChanged イベントを発生させます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected void RaisePropertyChanged([CallerMemberName] string name = null)
+        {
+            var e = new PropertyChangedEventArgs(name);
+            if (_context != null)
+            {
+                if (IsSynchronous) _context.Send(_ => OnPropertyChanged(e), null);
+                else _context.Post(_ => OnPropertyChanged(e), null);
+            }
+            else OnPropertyChanged(e);
+        }
+
+        #endregion
+
+        #region Methods
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// RaiseValueChanged
+        ///
+        /// <summary>
+        /// Value の内容が変化した事を表すイベントを発生させます。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public void RaiseValueChanged() => RaisePropertyChanged(nameof(Value));
+
+        #endregion
+
+        #region Fields
+        private readonly SynchronizationContext _context;
+        #endregion
+    }
+}
---
+++ Tests/Sources/BindableFuncTest.cs
@@ -0,0 +1,115 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+/* ------------------------------------------------------------------------- */
+using NUnit.Framework;
+using System;
+using System.Threading;
+
+namespace Cube.Xui.Tests
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// BindableFuncTest
+    ///
+    /// <summary>
+    /// BindableFunc のテスト用クラスです。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    [TestFixture]
+    class BindableFuncTest
+    {
+        #region Tests
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// GetValue
+        ///
+        /// <summary>
+        /// Value を取得するテストを実行します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [Test]
+        public void GetValue()
+        {
+            var count = 0;
+            var dest  = new BindableFunc<int>(() => count);
+
+            dest.PropertyChanged += (s, e) => ++count;
+            Assert.That(dest.IsSynchronous, Is.True);
+            Assert.That(dest.HasValue,      Is.True);
+            Assert.That(dest.Value,         Is.EqualTo(0));
+
+            dest.RaiseValueChanged();
+            Assert.That(dest.IsSynchronous, Is.True);
+            Assert.That(dest.HasValue,      Is.True);
+            Assert.That(dest.Value,         Is.EqualTo(1));
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// GetValue_Context
+        ///
+        /// <summary>
+        /// SynchronizationContext を指定した時の挙動を確認します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [Test]
+        public void GetValue_Context()
+        {
+            var count = 0;
+            var dest  = new BindableFunc<int>(() => count, new SynchronizationContext());
+
+            dest.PropertyChanged += (s, e) => ++count;
+            Assert.That(dest.IsSynchronous, Is.True);
+            Assert.That(dest.HasValue,      Is.True);
+            Assert.That(dest.Value,         Is.EqualTo(0));
+
+            dest.RaiseValueChanged();
+            Assert.That(dest.IsSynchronous, Is.True);
+            Assert.That(dest.HasValue,      Is.True);
+            Assert.That(dest.Value,         Is.EqualTo(1));
+
+            dest.IsSynchronous = false;
+            dest.RaiseValueChanged();
+            Assert.That(dest.IsSynchronous, Is.False);
+            Assert.That(dest.HasValue,      Is.True);
+            Assert.That(dest.Value,         Is.AtLeast(1));
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// GetValue_Throws
+        ///
+        /// <summary>
+        /// 無効な関数オブジェクトを指定した時の挙動を確認します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [Test]
+        public void GetValue_Throws()
+        {
+            var dest = new BindableFunc<int>(null);
+            Assert.That(dest.HasValue, Is.False);
+            Assert.That(() => dest.Value, Throws.TypeOf<NullReferenceException>());
+        }
+
+        #endregion
+    }
+}
