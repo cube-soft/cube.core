41a9e364cfb3b009efda8e51ee225b2933c9a1ce
clown <tt.clown@gmail.com>
2018-09-19 12:53:33 +0900

    Add CommandBehavior.

---
+++ Libraries/Sources/Behaviors/CommandBehavior.cs
@@ -0,0 +1,67 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+/* ------------------------------------------------------------------------- */
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace Cube.Xui.Behaviors
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// CommandBehavior
+    ///
+    /// <summary>
+    /// Represents the inherited Behavior(T) class that has a Command
+    /// bindable property.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public class CommandBehavior<T> : Behavior<T> where T : DependencyObject
+    {
+        #region Properties
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Command
+        ///
+        /// <summary>
+        /// Gets or sets the command.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public ICommand Command
+        {
+            get => GetValue(CommandProperty) as ICommand;
+            set => SetValue(CommandProperty, value);
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// CommandProperty
+        ///
+        /// <summary>
+        /// Gets the DependencyProperty object for the Command property.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public static readonly DependencyProperty CommandProperty =
+            DependencyFactory.Create<CommandBehavior<T>, ICommand>(nameof(Command), (s, e) => s.Command = e);
+
+        #endregion
+    }
+}
---
+++ Libraries/Sources/DependencyFactory.cs
@@ -0,0 +1,69 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+/* ------------------------------------------------------------------------- */
+using System;
+using System.Windows;
+
+namespace Cube.Xui
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// DependencyFactory
+    ///
+    /// <summary>
+    /// Provides functionality to create DependencyProperty objects.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public static class DependencyFactory
+    {
+        #region Methods
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Create
+        ///
+        /// <summary>
+        /// Creates a new instance of the DependencyProperty class
+        /// with the specified types and arguments.
+        /// </summary>
+        ///
+        /// <typeparam name="TOwner">Owner type.</typeparam>
+        /// <typeparam name="TProperty">Property type.</typeparam>
+        ///
+        /// <param name="name">Property name.</param>
+        /// <param name="action">Action to set a new value.</param>
+        ///
+        /// <returns>DependencyProperty object.</returns>
+        ///
+        /* ----------------------------------------------------------------- */
+        public static DependencyProperty Create<TOwner, TProperty>(
+            string name, Action<TOwner, TProperty> action)
+            where TOwner : DependencyObject =>
+            DependencyProperty.RegisterAttached(
+                name,
+                typeof(TProperty),
+                typeof(TOwner),
+                new PropertyMetadata(default(TProperty), (s, e) =>
+                {
+                    if (s is TOwner owner && e.NewValue is TProperty value) action(owner, value);
+                })
+            );
+
+        #endregion
+    }
+}
