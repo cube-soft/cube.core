7e83be56b8d4dd5f7f758aae3e92a4146a9203fb
clown <tt.clown@gmail.com>
2018-09-19 19:55:56 +0900

    Add ClosingBehavior.

--- Libraries/Sources/Behaviors/CloseBehavior.cs
+++ Libraries/Sources/Behaviors/CloseBehavior.cs
@@ -15,10 +15,13 @@
 // limitations under the License.
 //
 /* ------------------------------------------------------------------------- */
+using System.ComponentModel;
 using System.Windows;
 
 namespace Cube.Xui.Behaviors
 {
+    #region CloseBehavior
+
     /* --------------------------------------------------------------------- */
     ///
     /// CloseBehavior
@@ -48,4 +51,71 @@ namespace Cube.Xui.Behaviors
 
         #endregion
     }
+
+    #endregion
+
+    #region ClosingBehavior
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// ClosingBehavior
+    ///
+    /// <summary>
+    /// Represents the behavior when the Closing event is fired.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public class ClosingBehavior : CommandBehavior<Window>
+    {
+        #region Implementations
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// OnAttached
+        ///
+        /// <summary>
+        /// Occurs when the instance is attached to the Window.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.Closing -= WhenClosing;
+            AssociatedObject.Closing += WhenClosing;
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// OnDetaching
+        ///
+        /// <summary>
+        /// Occurs when the instance is detaching from the Window.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected override void OnDetaching()
+        {
+            AssociatedObject.Closing -= WhenClosing;
+            base.OnDetaching();
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// WhenClosing
+        ///
+        /// <summary>
+        /// Occurs when the Closing event is fired.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private void WhenClosing(object sender, CancelEventArgs e)
+        {
+            if (Command?.CanExecute(e) ?? false) Command.Execute(e);
+        }
+
+        #endregion
+    }
+
+    #endregion
 }
