9bbb03e664519d7274cc84db6effa51cca3514a9
clown <tt.clown@gmail.com>
2018-10-05 23:35:36 +0900

    Fix settings.

--- Libraries/Properties/AssemblyInfo.cs
+++ Libraries/Properties/AssemblyInfo.cs
@@ -2,15 +2,15 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+[assembly: AssemblyVersion("1.12.0.0")]
+[assembly: AssemblyFileVersion("1.12.0.0")]
 [assembly: AssemblyTitle("Cube.FileSystem")]
 [assembly: AssemblyDescription("I/O library for CubeSoft applications.")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CubeSoft")]
 [assembly: AssemblyProduct("Cube.FileSystem")]
 [assembly: AssemblyCopyright("Copyright © 2010 CubeSoft, Inc.")]
 [assembly: AssemblyTrademark("")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("e89aad2c-5a74-49f6-ad84-65f0016ef1bb")]
-[assembly: AssemblyVersion("1.12.0.0")]
-[assembly: AssemblyFileVersion("1.12.0.0")]
