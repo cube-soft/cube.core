e1372984b4152c2ef2fb24411fb4e5393e149494
clown <tt.clown@gmail.com>
2019-04-23 22:47:25 +0900

    Fix namespace.

--- Tests/Sources/Mixin/EnumerableTest.cs
+++ Tests/Sources/Mixin/EnumerableTest.cs
@@ -21,7 +21,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Cube.Tests
+namespace Cube.Tests.Mixin
 {
     /* --------------------------------------------------------------------- */
     ///
