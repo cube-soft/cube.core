0513e1c9887cf16c27657ccbb4b75cfb90b175ec
clown <tt.clown@gmail.com>
2019-05-06 22:50:47 +0900

    Add Send and Post methods.

--- Libraries/Sources/Dispatcher.cs
+++ Libraries/Sources/Dispatcher.cs
@@ -162,9 +162,35 @@ namespace Cube
         /* ----------------------------------------------------------------- */
         public void Invoke(Action action)
         {
-            if (Synchronous) Context.Send(e => action(), null);
-            else Context.Post(e => action(), null);
+            if (Synchronous) Send(action);
+            else Post(action);
         }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Send
+        ///
+        /// <summary>
+        /// Sends the specified action with the Synchronization context.
+        /// </summary>
+        ///
+        /// <param name="action">Invoked action.</param>
+        ///
+        /* ----------------------------------------------------------------- */
+        public void Send(Action action) => Context.Send(e => action(), null);
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Post
+        ///
+        /// <summary>
+        /// Posts the specified action with the Synchronization context.
+        /// </summary>
+        ///
+        /// <param name="action">Invoked action.</param>
+        ///
+        /* ----------------------------------------------------------------- */
+        public void Post(Action action) => Context.Post(e => action(), null);
 
         #endregion
 
