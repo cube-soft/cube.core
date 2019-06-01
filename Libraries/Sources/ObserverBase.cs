4dabbc8d8252af95f1fa9642b7204dfd142fb675
clown <tt.clown@gmail.com>
2019-06-01 14:56:43 +0900

    Add IObservePropertyChanged and related classes.

---
+++ Libraries/Sources/ObserverBase.cs
@@ -0,0 +1,153 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+/* ------------------------------------------------------------------------- */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Cube
+{
+    #region IObservePropertyChnaged
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// IObservePropertyChnaged
+    ///
+    /// <summary>
+    /// Provides interface to observe the PropertyChanged event of
+    /// INotifyPropertyChanged objects.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public interface IObservePropertyChnaged
+    {
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Observe
+        ///
+        /// <summary>
+        /// Observes the PropertyChanged event of the specified object.
+        /// </summary>
+        ///
+        /// <param name="src">Observed object.</param>
+        /// <param name="names">Target property names.</param>
+        ///
+        /* ----------------------------------------------------------------- */
+        void Observe(INotifyPropertyChanged src, params string[] names);
+    }
+
+    #endregion
+
+    #region ObserverBase
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// ObserverBase
+    ///
+    /// <summary>
+    /// Represents a base class of the IObservePropertyChnaged interface.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public abstract class ObserverBase : DisposableBase, IObservePropertyChnaged
+    {
+        #region Constructors
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// ObserverBase
+        ///
+        /// <summary>
+        /// Initializes a new instance of the ObserverBase class.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected ObserverBase() { }
+
+        #endregion
+
+        #region Methods
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Observe
+        ///
+        /// <summary>
+        /// Observes the PropertyChanged event of the specified object.
+        /// </summary>
+        ///
+        /// <param name="src">Observed object.</param>
+        /// <param name="names">Target property names.</param>
+        ///
+        /* ----------------------------------------------------------------- */
+        public void Observe(INotifyPropertyChanged src, params string[] names)
+        {
+            var set = new HashSet<string>(names);
+            void handler(object s, PropertyChangedEventArgs e)
+            {
+                if (set.Count <= 0 || set.Contains(e.PropertyName)) React();
+            }
+
+            src.PropertyChanged += handler;
+            _observer.Add(Disposable.Create(() => src.PropertyChanged -= handler));
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// React
+        ///
+        /// <summary>
+        /// Invokes when the PropertyChanged event of an observed object
+        /// is fired.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected abstract void React();
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Dispose
+        ///
+        /// <summary>
+        /// Releases the unmanaged resources used by the object and
+        /// optionally releases the managed resources.
+        /// </summary>
+        ///
+        /// <param name="disposing">
+        /// true to release both managed and unmanaged resources;
+        /// false to release only unmanaged resources.
+        /// </param>
+        ///
+        /* ----------------------------------------------------------------- */
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                foreach (var obj in _observer) obj.Dispose();
+                _observer.Clear();
+            }
+        }
+
+        #endregion
+
+        #region Fields
+        private readonly List<IDisposable> _observer = new List<IDisposable>();
+        #endregion
+    }
+
+    #endregion
+}
---
+++ Tests/Sources/ObserverTest.cs
@@ -0,0 +1,104 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+/* ------------------------------------------------------------------------- */
+using Cube.Mixin.Observer;
+using NUnit.Framework;
+
+namespace Cube.Tests
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// ObserverTest
+    ///
+    /// <summary>
+    /// Test the IObservePropertyChanged implemented class.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    [TestFixture]
+    class ObserverTest
+    {
+        #region Tests
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Observe
+        ///
+        /// <summary>
+        /// Tests Observe and related methods.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [Test]
+        public void Observe()
+        {
+            var n   = new Accessor<int>();
+            var src = new Person();
+
+            using (new Mock(n)
+                .Associate(src) // All
+                .Associate(src, nameof(src.Name))
+            ) {
+                Assert.That(src.Age,  Is.EqualTo(0));
+                src.Age = 10;
+                Assert.That(n.Get(),  Is.EqualTo(1));
+                Assert.That(src.Age,  Is.EqualTo(10));
+                src.Age = 10;
+                Assert.That(n.Get(),  Is.EqualTo(1));
+                Assert.That(src.Age,  Is.EqualTo(10));
+
+                Assert.That(src.Name, Is.Empty);
+                src.Name = "Test";
+                Assert.That(n.Get(),  Is.EqualTo(3));
+                Assert.That(src.Name, Is.EqualTo("Test"));
+                src.Name = "Test";
+                Assert.That(n.Get(),  Is.EqualTo(3));
+                Assert.That(src.Name, Is.EqualTo("Test"));
+            }
+
+            src.Age = 20;
+            Assert.That(n.Get(),  Is.EqualTo(3));
+            Assert.That(src.Age,  Is.EqualTo(20));
+
+            src.Name = "";
+            Assert.That(n.Get(),  Is.EqualTo(3));
+            Assert.That(src.Name, Is.Empty);
+        }
+
+        #endregion
+
+        #region Others
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Mock
+        ///
+        /// <summary>
+        /// Represents an ObserverBase inherited class for testing.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private class Mock : ObserverBase
+        {
+            public Mock(Accessor<int> count) { _count = count; }
+            protected override void React() => _count.Set(_count.Get() + 1);
+            private readonly Accessor<int> _count;
+        }
+
+        #endregion
+    }
+}
