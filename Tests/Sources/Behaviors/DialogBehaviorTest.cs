b3f31476d967162c093103790e7bd5f5ec75f215
clown <tt.clown@gmail.com>
2019-07-14 21:17:59 +0900

    Add tests for DialogBehavior.

---
+++ Tests/Sources/Behaviors/DialogBehaviorTest.cs
@@ -0,0 +1,68 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+/* ------------------------------------------------------------------------- */
+using Cube.Xui.Behaviors;
+using NUnit.Framework;
+using System.Threading;
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace Cube.Xui.Tests.Behaviors
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// DialogBehaviorTest
+    ///
+    /// <summary>
+    /// Tests for the DialogBehavior, OpenFileBehavior, SaveFileBehavior,
+    /// and OpenDirectoryBehavior classes.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    [TestFixture]
+    [Apartment(ApartmentState.STA)]
+    class DialogBehaviorTest : ViewFixture
+    {
+        #region Tests
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Create
+        ///
+        /// <summary>
+        /// Tests to create behaviors.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [Test]
+        public void Create()
+        {
+            var view = new Window();
+            var src  = new Behavior<FrameworkElement>[]
+            {
+                Attach(view, new DialogBehavior()),
+                Attach(view, new OpenDirectoryBehavior()),
+                Attach(view, new OpenFileBehavior()),
+                Attach(view, new SaveFileBehavior())
+            };
+
+            foreach (var obj in src) obj.Detach();
+        }
+
+        #endregion
+    }
+}
