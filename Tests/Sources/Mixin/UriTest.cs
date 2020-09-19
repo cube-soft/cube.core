c2ff85d1698c7b3b3f918b5e9a255c4a52c0d507
clown <tt.clown@gmail.com>
2020-09-19 21:11:38 +0900

    Fix version number.

--- Tests/Sources/Mixin/UriTest.cs
+++ Tests/Sources/Mixin/UriTest.cs
@@ -158,7 +158,7 @@ namespace Cube.Tests.Mixin
         public void With_SoftwareVersion()
         {
             var asm  = typeof(UriTest).Assembly;
-            var dest = $"{Create()}?ver=2.1beta";
+            var dest = $"{Create()}?ver=2.2beta";
             var src  = Create().With(new SoftwareVersion(asm)
             {
                 Digit = 2,
