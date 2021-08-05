00e5f9f241ff274143d4261dfd09f33d1c4b26e0
clown <tt.clown@gmail.com>
2021-08-06 07:35:36 +0900

    Fix setter as protected.

--- Libraries/FileSystem/Sources/Io/EntitySource.cs
+++ Libraries/FileSystem/Sources/Io/EntitySource.cs
@@ -94,7 +94,7 @@ namespace Cube.FileSystem
         /// </summary>
         ///
         /* ----------------------------------------------------------------- */
-        public bool Exists { get; set; }
+        public bool Exists { get; protected set; }
 
         /* ----------------------------------------------------------------- */
         ///
@@ -106,7 +106,7 @@ namespace Cube.FileSystem
         /// </summary>
         ///
         /* ----------------------------------------------------------------- */
-        public bool IsDirectory { get; set; }
+        public bool IsDirectory { get; protected set; }
 
         /* ----------------------------------------------------------------- */
         ///
@@ -117,7 +117,7 @@ namespace Cube.FileSystem
         /// </summary>
         ///
         /* ----------------------------------------------------------------- */
-        public string Name { get; set; }
+        public string Name { get; protected set; }
 
         /* ----------------------------------------------------------------- */
         ///
@@ -128,7 +128,7 @@ namespace Cube.FileSystem
         /// </summary>
         ///
         /* ----------------------------------------------------------------- */
-        public string BaseName { get; set; }
+        public string BaseName { get; protected set; }
 
         /* ----------------------------------------------------------------- */
         ///
@@ -139,7 +139,7 @@ namespace Cube.FileSystem
         /// </summary>
         ///
         /* ----------------------------------------------------------------- */
-        public string Extension { get; set; }
+        public string Extension { get; protected set; }
 
         /* ----------------------------------------------------------------- */
         ///
@@ -150,7 +150,7 @@ namespace Cube.FileSystem
         /// </summary>
         ///
         /* ----------------------------------------------------------------- */
-        public string FullName { get; set; }
+        public string FullName { get; protected set; }
 
         /* ----------------------------------------------------------------- */
         ///
@@ -161,7 +161,7 @@ namespace Cube.FileSystem
         /// </summary>
         ///
         /* ----------------------------------------------------------------- */
-        public string DirectoryName { get; set; }
+        public string DirectoryName { get; protected set; }
 
         /* ----------------------------------------------------------------- */
         ///
@@ -172,7 +172,7 @@ namespace Cube.FileSystem
         /// </summary>
         ///
         /* ----------------------------------------------------------------- */
-        public long Length { get; set; }
+        public long Length { get; protected set; }
 
         /* ----------------------------------------------------------------- */
         ///
@@ -183,7 +183,7 @@ namespace Cube.FileSystem
         /// </summary>
         ///
         /* ----------------------------------------------------------------- */
-        public FileAttributes Attributes { get; set; }
+        public FileAttributes Attributes { get; protected set; }
 
         /* ----------------------------------------------------------------- */
         ///
@@ -194,7 +194,7 @@ namespace Cube.FileSystem
         /// </summary>
         ///
         /* ----------------------------------------------------------------- */
-        public DateTime CreationTime { get; set; }
+        public DateTime CreationTime { get; protected set; }
 
         /* ----------------------------------------------------------------- */
         ///
@@ -205,7 +205,7 @@ namespace Cube.FileSystem
         /// </summary>
         ///
         /* ----------------------------------------------------------------- */
-        public DateTime LastWriteTime { get; set; }
+        public DateTime LastWriteTime { get; protected set; }
 
         /* ----------------------------------------------------------------- */
         ///
@@ -216,7 +216,7 @@ namespace Cube.FileSystem
         /// </summary>
         ///
         /* ----------------------------------------------------------------- */
-        public DateTime LastAccessTime { get; set; }
+        public DateTime LastAccessTime { get; protected set; }
 
         #endregion
 
