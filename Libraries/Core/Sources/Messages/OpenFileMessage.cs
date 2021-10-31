c0c3277be9f78b0a8a13bc6172caff41f330ad08
clown <tt.clown@gmail.com>
2021-10-31 18:11:35 +0900

    Fix constructors of file messages.

---
+++ Libraries/Core/Sources/Messages/OpenFileMessage.cs
@@ -0,0 +1,105 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+/* ------------------------------------------------------------------------- */
+using System.Collections.Generic;
+using System.Linq;
+using Cube.FileSystem;
+using Cube.Mixin.Collections;
+using Cube.Mixin.String;
+
+namespace Cube
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// OpenFileMessage
+    ///
+    /// <summary>
+    /// Represents information to show the OpenFileDialog.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public class OpenFileMessage : OpenOrSaveFileMessage<IEnumerable<string>>
+    {
+        #region Constructors
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// OpenFileMessage
+        ///
+        /// <summary>
+        /// Initializes a new instance of the OpenFileMessage class.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public OpenFileMessage() : this(string.Empty) { }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// OpenFileMessage
+        ///
+        /// <summary>
+        /// Initializes a new instance of the OpenFileMessage class with
+        /// the specified arguments.
+        /// </summary>
+        ///
+        /// <param name="src">Initial path.</param>
+        ///
+        /* ----------------------------------------------------------------- */
+        public OpenFileMessage(string src) : this(src.HasValue() ? Io.Get(src) : default) { }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// OpenFileMessage
+        ///
+        /// <summary>
+        /// Initializes a new instance of the OpenFileMessage class with
+        /// the specified arguments.
+        /// </summary>
+        ///
+        /// <param name="src">Entity for the initial path.</param>
+        ///
+        /* ----------------------------------------------------------------- */
+        public OpenFileMessage(Entity src)
+        {
+            CheckPathExists = true;
+            if (src != null)
+            {
+                Value = src.Name.ToEnumerable();
+                InitialDirectory = src.DirectoryName;
+            }
+            else Value = Enumerable.Empty<string>();
+        }
+
+        #endregion
+
+        #region Properties
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Multiselect
+        ///
+        /// <summary>
+        /// Gets or sets an option indicating whether the OpenFileDialog
+        /// allows users to select multiple files.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public bool Multiselect { get; set; }
+
+        #endregion
+    }
+}
---
+++ Libraries/Core/Sources/Messages/OpenOrSaveFileMessage.cs
@@ -0,0 +1,84 @@
+﻿/* ------------------------------------------------------------------------- */
+//
+// Copyright (c) 2010 CubeSoft, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+/* ------------------------------------------------------------------------- */
+namespace Cube
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// OpenOrSaveFileMessage(TValue)
+    ///
+    /// <summary>
+    /// Represents shared information to show either the OpenFileDialog
+    /// or SaveFileDialog.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public abstract class OpenOrSaveFileMessage<TValue> : CancelMessage<TValue>
+    {
+        #region Properties
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// CheckPathExists
+        ///
+        /// <summary>
+        /// Gets or sets a value indicating whether a file dialog
+        /// displays a warning if the user specifies a file name that
+        /// does not exist.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public bool CheckPathExists { get; set; }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// InitialDirectory
+        ///
+        /// <summary>
+        /// Gets or sets the initial directory that is displayed by a file
+        /// dialog.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public string InitialDirectory { get; set; } = string.Empty;
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Filter
+        ///
+        /// <summary>
+        /// Gets or sets the filter string that determines what types of
+        /// files are displayed from a from dialog.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public string Filter { get; set; } = "All Files (*.*)|*.*";
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// FilterIndex
+        ///
+        /// <summary>
+        /// Gets or sets a value to select the initial filter.
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public int FilterIndex { get; set; }
+
+        #endregion
+    }
+}
