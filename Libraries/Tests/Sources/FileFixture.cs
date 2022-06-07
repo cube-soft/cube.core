32116a1d0efc8ef144cdbee61219c626cc8c9580
clown <tt.clown@gmail.com>
2022-06-07 16:01:27 +0900

    C# 10 base refactoring to the Cube.Tests project.

--- Libraries/Tests/Sources/FileFixture.cs
+++ Libraries/Tests/Sources/FileFixture.cs
@@ -15,172 +15,171 @@
 // limitations under the License.
 //
 /* ------------------------------------------------------------------------- */
+namespace Cube.Tests;
+
 using System.Reflection;
 using Cube.FileSystem;
 using Cube.Mixin.Assembly;
 using Cube.Mixin.Syntax;
 using NUnit.Framework;
 
-namespace Cube.Tests
+/* ------------------------------------------------------------------------- */
+///
+/// FileFixture
+///
+/// <summary>
+/// Provides functionality to load or save files for tests.
+/// </summary>
+///
+/* ------------------------------------------------------------------------- */
+public abstract class FileFixture
 {
+    #region Constructors
+
     /* --------------------------------------------------------------------- */
     ///
     /// FileFixture
     ///
     /// <summary>
-    /// Provides functionality to load or save files for tests.
+    /// Initializes a new instance of the FileFixture class.
     /// </summary>
     ///
     /* --------------------------------------------------------------------- */
-    public abstract class FileFixture
+    protected FileFixture()
     {
-        #region Constructors
+        Root     = Assembly.GetExecutingAssembly().GetDirectoryName();
+        Name     = GetType().FullName;
+        Examples = Io.Combine(Root, nameof(Examples));
+        Results  = Io.Combine(Root, nameof(Results), Name);
 
-        /* ----------------------------------------------------------------- */
-        ///
-        /// FileFixture
-        ///
-        /// <summary>
-        /// Initializes a new instance of the FileFixture class.
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        protected FileFixture()
-        {
-            Root     = Assembly.GetExecutingAssembly().GetDirectoryName();
-            Name     = GetType().FullName;
-            Examples = Io.Combine(Root, nameof(Examples));
-            Results  = Io.Combine(Root, nameof(Results), Name);
-
-            Io.CreateDirectory(Results);
-            Delete(Results);
-        }
-
-        #endregion
-
-        #region Properties
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Root
-        ///
-        /// <summary>
-        /// Gets the path of the root directory that has test resources.
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        protected string Root { get; }
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Examples
-        ///
-        /// <summary>
-        /// Gets the path of the directory that has example files for tests.
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        protected string Examples { get; }
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Results
-        ///
-        /// <summary>
-        /// Gets the path of the directory that test results is saved.
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        protected string Results { get; }
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Name
-        ///
-        /// <summary>
-        /// Gets the class name.
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        protected string Name { get; }
-
-        #endregion
-
-        #region Methods
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Get
-        ///
-        /// <summary>
-        /// Gets the absolute path with the specified file or directory,
-        /// assuming that it is in the Results directory.
-        /// </summary>
-        ///
-        /// <param name="paths">
-        /// List of file or directory names to be combined as a path.
-        /// </param>
-        ///
-        /// <returns>Combined path.</returns>
-        ///
-        /* ----------------------------------------------------------------- */
-        protected string Get(params string[] paths) =>
-            Io.Combine(Results, Io.Combine(paths));
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// GetSource
-        ///
-        /// <summary>
-        /// Gets the absolute path with the specified file or directory,
-        /// assuming that it is in the Examples directory.
-        /// </summary>
-        ///
-        /// <param name="paths">
-        /// List of file or directory names to be combined as a path.
-        /// </param>
-        ///
-        /// <returns>Combined path.</returns>
-        ///
-        /* ----------------------------------------------------------------- */
-        protected string GetSource(params string[] paths) =>
-            Io.Combine(Examples, Io.Combine(paths));
-
-        #endregion
-
-        #region Methods
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Teardown
-        ///
-        /// <summary>
-        /// Invokes the tear-down operation.
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        [TearDown]
-        protected virtual void Teardown() => Io.Configure(new());
-
-        #endregion
-
-        #region Implementations
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Delete
-        ///
-        /// <summary>
-        /// Deletes all of files and directories in the specified path.
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        private void Delete(string directory)
-        {
-            Io.GetFiles(directory).Each(f => Io.Delete(f));
-            Io.GetDirectories(directory).Each(d => { Delete(d); Io.Delete(d); });
-        }
-
-        #endregion
+        Io.CreateDirectory(Results);
+        Delete(Results);
     }
+
+    #endregion
+
+    #region Properties
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Root
+    ///
+    /// <summary>
+    /// Gets the path of the root directory that has test resources.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    protected string Root { get; }
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Examples
+    ///
+    /// <summary>
+    /// Gets the path of the directory that has example files for tests.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    protected string Examples { get; }
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Results
+    ///
+    /// <summary>
+    /// Gets the path of the directory that test results is saved.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    protected string Results { get; }
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Name
+    ///
+    /// <summary>
+    /// Gets the class name.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    protected string Name { get; }
+
+    #endregion
+
+    #region Methods
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Get
+    ///
+    /// <summary>
+    /// Gets the absolute path with the specified file or directory,
+    /// assuming that it is in the Results directory.
+    /// </summary>
+    ///
+    /// <param name="paths">
+    /// List of file or directory names to be combined as a path.
+    /// </param>
+    ///
+    /// <returns>Combined path.</returns>
+    ///
+    /* --------------------------------------------------------------------- */
+    protected string Get(params string[] paths) =>
+        Io.Combine(Results, Io.Combine(paths));
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// GetSource
+    ///
+    /// <summary>
+    /// Gets the absolute path with the specified file or directory,
+    /// assuming that it is in the Examples directory.
+    /// </summary>
+    ///
+    /// <param name="paths">
+    /// List of file or directory names to be combined as a path.
+    /// </param>
+    ///
+    /// <returns>Combined path.</returns>
+    ///
+    /* --------------------------------------------------------------------- */
+    protected string GetSource(params string[] paths) =>
+        Io.Combine(Examples, Io.Combine(paths));
+
+    #endregion
+
+    #region Methods
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Teardown
+    ///
+    /// <summary>
+    /// Invokes the tear-down operation.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    [TearDown]
+    protected virtual void Teardown() => Io.Configure(new());
+
+    #endregion
+
+    #region Implementations
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Delete
+    ///
+    /// <summary>
+    /// Deletes all of files and directories in the specified path.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    private void Delete(string directory)
+    {
+        Io.GetFiles(directory).Each(f => Io.Delete(f));
+        Io.GetDirectories(directory).Each(d => { Delete(d); Io.Delete(d); });
+    }
+
+    #endregion
 }
