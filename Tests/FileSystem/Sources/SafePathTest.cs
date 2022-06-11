4abbdfb58ac8a669bfb8d1a8a6e276f4be1ed205
clown <tt.clown@gmail.com>
2022-06-11 18:36:18 +0900

    C# 10 base refactoring to the Cube.FileSystem.Tests project.

--- Tests/FileSystem/Sources/SafePathTest.cs
+++ Tests/FileSystem/Sources/SafePathTest.cs
@@ -15,157 +15,154 @@
 // limitations under the License.
 //
 /* ------------------------------------------------------------------------- */
+namespace Cube.FileSystem.Tests;
+
 using System.Linq;
 using NUnit.Framework;
 
-namespace Cube.FileSystem.Tests
+/* ------------------------------------------------------------------------- */
+///
+/// SafePathTest
+///
+/// <summary>
+/// Tests the SafePath class.
+/// </summary>
+///
+/* ------------------------------------------------------------------------- */
+[TestFixture]
+class SafePathTest
 {
+    #region Tests
+
     /* --------------------------------------------------------------------- */
     ///
-    /// SafePathTest
+    /// Escape
     ///
     /// <summary>
-    /// Tests the SafePath class.
+    /// Tests the escaping process.
     /// </summary>
     ///
     /* --------------------------------------------------------------------- */
-    [TestFixture]
-    class SafePathTest
+    [TestCase(@"C:\windows\dir\file.txt",       '_', 4, ExpectedResult = @"C:\windows\dir\file.txt")]
+    [TestCase(@"C:\windows\dir\file*?<>|.txt",  '_', 4, ExpectedResult = @"C:\windows\dir\file_____.txt")]
+    [TestCase(@"C:\windows\dir\file:.txt",      '+', 4, ExpectedResult = @"C:\windows\dir\file+.txt")]
+    [TestCase(@"C:\windows\dir:\file.txt",      '+', 4, ExpectedResult = @"C:\windows\dir+\file.txt")]
+    [TestCase(@"C:\windows\dir\\file.txt.",     '+', 4, ExpectedResult = @"C:\windows\dir\file.txt")]
+    [TestCase(@"C:\windows\dir\file.txt.",      '+', 4, ExpectedResult = @"C:\windows\dir\file.txt")]
+    [TestCase(@"C:\windows\dir\file.txt. ... ", '+', 4, ExpectedResult = @"C:\windows\dir\file.txt")]
+    [TestCase(@"C:\CON\PRN\AUX.txt",            '_', 4, ExpectedResult = @"C:\_CON\_PRN\_AUX.txt")]
+    [TestCase(@"C:\COM0\com1\Com2.txt",         '_', 4, ExpectedResult = @"C:\_COM0\_com1\_Com2.txt")]
+    [TestCase(@"C:\LPT1\LPT10\LPT5.txt",        '_', 4, ExpectedResult = @"C:\_LPT1\LPT10\_LPT5.txt")]
+    [TestCase(@"C:\LPT2\:LPT3:\LPT4.txt",       '_', 4, ExpectedResult = @"C:\_LPT2\_LPT3_\_LPT4.txt")]
+    [TestCase(@"/unix/foo/bar.txt",             '_', 3, ExpectedResult = @"unix\foo\bar.txt")]
+    [TestCase(@"",                              '_', 0, ExpectedResult = @"")]
+    [TestCase(null,                             '_', 0, ExpectedResult = @"")]
+    public string Escape(string src, char replaced, int count)
     {
-        #region Tests
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Escape
-        ///
-        /// <summary>
-        /// Tests the escaping process.
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        [TestCase(@"C:\windows\dir\file.txt",       '_', 4, ExpectedResult = @"C:\windows\dir\file.txt")]
-        [TestCase(@"C:\windows\dir\file*?<>|.txt",  '_', 4, ExpectedResult = @"C:\windows\dir\file_____.txt")]
-        [TestCase(@"C:\windows\dir\file:.txt",      '+', 4, ExpectedResult = @"C:\windows\dir\file+.txt")]
-        [TestCase(@"C:\windows\dir:\file.txt",      '+', 4, ExpectedResult = @"C:\windows\dir+\file.txt")]
-        [TestCase(@"C:\windows\dir\\file.txt.",     '+', 4, ExpectedResult = @"C:\windows\dir\file.txt")]
-        [TestCase(@"C:\windows\dir\file.txt.",      '+', 4, ExpectedResult = @"C:\windows\dir\file.txt")]
-        [TestCase(@"C:\windows\dir\file.txt. ... ", '+', 4, ExpectedResult = @"C:\windows\dir\file.txt")]
-        [TestCase(@"C:\CON\PRN\AUX.txt",            '_', 4, ExpectedResult = @"C:\_CON\_PRN\_AUX.txt")]
-        [TestCase(@"C:\COM0\com1\Com2.txt",         '_', 4, ExpectedResult = @"C:\_COM0\_com1\_Com2.txt")]
-        [TestCase(@"C:\LPT1\LPT10\LPT5.txt",        '_', 4, ExpectedResult = @"C:\_LPT1\LPT10\_LPT5.txt")]
-        [TestCase(@"C:\LPT2\:LPT3:\LPT4.txt",       '_', 4, ExpectedResult = @"C:\_LPT2\_LPT3_\_LPT4.txt")]
-        [TestCase(@"/unix/foo/bar.txt",             '_', 3, ExpectedResult = @"unix\foo\bar.txt")]
-        [TestCase(@"",                              '_', 0, ExpectedResult = @"")]
-        [TestCase(null,                             '_', 0, ExpectedResult = @"")]
-        public string Escape(string src, char replaced, int count)
+        var dest = new SafePath(src)
         {
-            var dest = new SafePath(src)
-            {
-                AllowDriveLetter      = true,
-                AllowParentDirectory  = false,
-                AllowCurrentDirectory = false,
-                AllowInactivation     = false,
-                AllowUnc              = false,
-                EscapeChar            = replaced,
-            };
+            AllowDriveLetter      = true,
+            AllowParentDirectory  = false,
+            AllowCurrentDirectory = false,
+            AllowInactivation     = false,
+            AllowUnc              = false,
+            EscapeChar            = replaced,
+        };
 
-            Assert.That(dest.Parts.Count(), Is.EqualTo(count));
-            return dest.Value;
-        }
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Escape_DriveLetter
-        ///
-        /// <summary>
-        /// Confirms the results according to the drive letter settings.
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        [TestCase(@"C:\windows\dir\allow.txt",    true,  ExpectedResult = @"C:\windows\dir\allow.txt")]
-        [TestCase(@"C:\C:\windows\dir\allow.txt", true,  ExpectedResult = @"C:\C_\windows\dir\allow.txt")]
-        [TestCase(@"C:\windows\dir\deny.txt",     false, ExpectedResult = @"C_\windows\dir\deny.txt")]
-        [TestCase(@"C:\C:\windows\dir\deny.txt",  false, ExpectedResult = @"C_\C_\windows\dir\deny.txt")]
-        public string Escape_DriveLetter(string src, bool drive) =>
-            new SafePath(src) { AllowDriveLetter = drive }.Value;
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Escape_CurrentDirectory
-        ///
-        /// <summary>
-        /// Confirms the results according to the "." letter settings.
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        [TestCase(@"C:\windows\dir\.\allow.txt", true, ExpectedResult = @"C:\windows\dir\.\allow.txt")]
-        [TestCase(@"C:\windows\dir\.\deny.txt", false, ExpectedResult = @"C:\windows\dir\deny.txt")]
-        public string Escape_CurrentDirectory(string src, bool allow) =>
-            new SafePath(src)
-            {
-                AllowInactivation     = false,
-                AllowCurrentDirectory = allow,
-            }.Value;
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Escape_ParentDirectory
-        ///
-        /// <summary>
-        /// Confirms the results according to the ".." letter settings.
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        [TestCase(@"C:\windows\dir\..\allow.txt", true, ExpectedResult = @"C:\windows\dir\..\allow.txt")]
-        [TestCase(@"C:\windows\dir\..\deny.txt", false, ExpectedResult = @"C:\windows\dir\deny.txt")]
-        public string Escape_ParentDirectory(string src, bool allow) =>
-            new SafePath(src)
-            {
-                AllowInactivation    = false,
-                AllowParentDirectory = allow,
-            }.Value;
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Escape_Inactivation
-        ///
-        /// <summary>
-        /// Confirms the results according to the inactivation settings.
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        [TestCase(@"\\?\C:\windows\dir\deny.txt",  false, ExpectedResult = @"C:\windows\dir\deny.txt")]
-        [TestCase(@"\\?\C:\windows\dir\allow.txt",  true, ExpectedResult = @"\\?\C:\windows\dir\allow.txt")]
-        [TestCase(@"\\?\C:\windows\.\..\allow.txt", true, ExpectedResult = @"\\?\C:\windows\allow.txt")]
-        public string Escape_Inactivation(string src, bool allow) =>
-            new SafePath(src)
-            {
-                AllowInactivation     = allow,
-                AllowDriveLetter      = true,
-                AllowCurrentDirectory = true,
-                AllowParentDirectory  = true,
-                AllowUnc              = true,
-            }.Value;
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Escape_Unc
-        ///
-        /// <summary>
-        /// Confirms the results according to the UNC path settings.
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        [TestCase(@"\\domain\dir\allow.txt", true, false, ExpectedResult = @"\\domain\dir\allow.txt")]
-        [TestCase(@"\\domain\dir\allow.txt", true,  true, ExpectedResult = @"domain\dir\allow.txt")]
-        [TestCase(@"\\domain\dir\deny.txt", false, false, ExpectedResult = @"domain\dir\deny.txt")]
-        public string Escape_Unc(string src, bool unc, bool inactivation) =>
-            new SafePath(src)
-            {
-                AllowInactivation = inactivation,
-                AllowUnc          = unc
-            }.Value;
-
-        #endregion
+        Assert.That(dest.Parts.Count(), Is.EqualTo(count));
+        return dest.Value;
     }
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Escape_DriveLetter
+    ///
+    /// <summary>
+    /// Confirms the results according to the drive letter settings.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    [TestCase(@"C:\windows\dir\allow.txt",    true,  ExpectedResult = @"C:\windows\dir\allow.txt")]
+    [TestCase(@"C:\C:\windows\dir\allow.txt", true,  ExpectedResult = @"C:\C_\windows\dir\allow.txt")]
+    [TestCase(@"C:\windows\dir\deny.txt",     false, ExpectedResult = @"C_\windows\dir\deny.txt")]
+    [TestCase(@"C:\C:\windows\dir\deny.txt",  false, ExpectedResult = @"C_\C_\windows\dir\deny.txt")]
+    public string Escape_DriveLetter(string src, bool drive) =>
+        new SafePath(src) { AllowDriveLetter = drive }.Value;
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Escape_CurrentDirectory
+    ///
+    /// <summary>
+    /// Confirms the results according to the "." letter settings.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    [TestCase(@"C:\windows\dir\.\allow.txt", true, ExpectedResult = @"C:\windows\dir\.\allow.txt")]
+    [TestCase(@"C:\windows\dir\.\deny.txt", false, ExpectedResult = @"C:\windows\dir\deny.txt")]
+    public string Escape_CurrentDirectory(string src, bool allow) =>
+        new SafePath(src)
+        {
+            AllowInactivation     = false,
+            AllowCurrentDirectory = allow,
+        }.Value;
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Escape_ParentDirectory
+    ///
+    /// <summary>
+    /// Confirms the results according to the ".." letter settings.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    [TestCase(@"C:\windows\dir\..\allow.txt", true, ExpectedResult = @"C:\windows\dir\..\allow.txt")]
+    [TestCase(@"C:\windows\dir\..\deny.txt", false, ExpectedResult = @"C:\windows\dir\deny.txt")]
+    public string Escape_ParentDirectory(string src, bool allow) =>
+        new SafePath(src)
+        {
+            AllowInactivation    = false,
+            AllowParentDirectory = allow,
+        }.Value;
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Escape_Inactivation
+    ///
+    /// <summary>
+    /// Confirms the results according to the inactivation settings.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    [TestCase(@"\\?\C:\windows\dir\deny.txt",  false, ExpectedResult = @"C:\windows\dir\deny.txt")]
+    [TestCase(@"\\?\C:\windows\dir\allow.txt",  true, ExpectedResult = @"\\?\C:\windows\dir\allow.txt")]
+    [TestCase(@"\\?\C:\windows\.\..\allow.txt", true, ExpectedResult = @"\\?\C:\windows\allow.txt")]
+    public string Escape_Inactivation(string src, bool allow) => new SafePath(src)
+    {
+        AllowInactivation     = allow,
+        AllowDriveLetter      = true,
+        AllowCurrentDirectory = true,
+        AllowParentDirectory  = true,
+        AllowUnc              = true,
+    }.Value;
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Escape_Unc
+    ///
+    /// <summary>
+    /// Confirms the results according to the UNC path settings.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    [TestCase(@"\\domain\dir\allow.txt", true, false, ExpectedResult = @"\\domain\dir\allow.txt")]
+    [TestCase(@"\\domain\dir\allow.txt", true,  true, ExpectedResult = @"domain\dir\allow.txt")]
+    [TestCase(@"\\domain\dir\deny.txt", false, false, ExpectedResult = @"domain\dir\deny.txt")]
+    public string Escape_Unc(string src, bool unc, bool inactivation) => new SafePath(src)
+    {
+        AllowInactivation = inactivation,
+        AllowUnc          = unc
+    }.Value;
+
+    #endregion
 }
--- Tests/FileSystem/Sources/StartupTest.cs
+++ Tests/FileSystem/Sources/StartupTest.cs
@@ -15,109 +15,108 @@
 // limitations under the License.
 //
 /* ------------------------------------------------------------------------- */
+namespace Cube.FileSystem.Tests;
+
 using Cube.Tests;
 using Microsoft.Win32;
 using NUnit.Framework;
 
-namespace Cube.FileSystem.Tests
+/* ------------------------------------------------------------------------- */
+///
+/// StartupTest
+///
+/// <summary>
+/// Tests the Startup class.
+/// </summary>
+///
+/* ------------------------------------------------------------------------- */
+[TestFixture]
+class StartupTest : FileFixture
 {
+    #region Tests
+
     /* --------------------------------------------------------------------- */
     ///
-    /// StartupTest
+    /// Create
     ///
     /// <summary>
-    /// Tests the Startup class.
+    /// Tests the constructor and properties.
     /// </summary>
     ///
     /* --------------------------------------------------------------------- */
-    [TestFixture]
-    class StartupTest : FileFixture
+    [Test]
+    public void Create()
     {
-        #region Tests
+        var src = new Startup(nameof(StartupTest));
+        Assert.That(src.Name,      Is.EqualTo(nameof(StartupTest)));
+        Assert.That(src.Enabled,   Is.False);
+        Assert.That(src.Source,    Is.EqualTo(string.Empty));
+        Assert.That(src.Arguments, Is.Not.Null);
+        Assert.That(src.Command,   Is.Empty);
 
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Create
-        ///
-        /// <summary>
-        /// Tests the constructor and properties.
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        [Test]
-        public void Create()
-        {
-            var src = new Startup(nameof(StartupTest));
-            Assert.That(src.Name,      Is.EqualTo(nameof(StartupTest)));
-            Assert.That(src.Enabled,   Is.False);
-            Assert.That(src.Source,    Is.EqualTo(string.Empty));
-            Assert.That(src.Arguments, Is.Not.Null);
-            Assert.That(src.Command,   Is.Empty);
-
-            src.Source = "path";
-            Assert.That(src.Command, Is.EqualTo("\"path\""));
-            src.Arguments.Add("arg1");
-            Assert.That(src.Command, Is.EqualTo("\"path\" \"arg1\""));
-            src.Arguments.Add("arg2");
-            Assert.That(src.Command, Is.EqualTo("\"path\" \"arg1\" \"arg2\""));
-            src.Source = string.Empty;
-            Assert.That(src.Command, Is.Empty);
-        }
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Create
-        ///
-        /// <summary>
-        /// Tests the Save method.
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        [Test]
-        public void Save()
-        {
-            var src = new Startup(nameof(StartupTest));
-            Assert.That(src.Enabled,      Is.False);
-            Assert.That(Exists(src.Name), Is.False, "Step1");
-
-            src.Enabled = true;
-            src.Save();
-            Assert.That(Exists(src.Name), Is.True,  "Step2");
-
-            src.Save(true);
-            Assert.That(Exists(src.Name), Is.False, "Step3");
-
-            src.Source = @"path\to\notfound.exe";
-            src.Save(false);
-            Assert.That(Exists(src.Name), Is.True,  "Step4");
-            src.Save(true);
-            Assert.That(Exists(src.Name), Is.False, "Step5");
-
-            src.Save();
-            Assert.That(Exists(src.Name), Is.True,  "Step6");
-            src.Enabled = false;
-            src.Save();
-            Assert.That(Exists(src.Name), Is.False, "Step7");
-        }
-
-        #endregion
-
-        #region Others
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// Exists
-        ///
-        /// <summary>
-        /// Determines whether the specified value name exists in the
-        /// registry.
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        private bool Exists(string name) => Registry.GetValue(
-            @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run", name, null
-        ) != null;
-
-        #endregion
+        src.Source = "path";
+        Assert.That(src.Command, Is.EqualTo("\"path\""));
+        src.Arguments.Add("arg1");
+        Assert.That(src.Command, Is.EqualTo("\"path\" \"arg1\""));
+        src.Arguments.Add("arg2");
+        Assert.That(src.Command, Is.EqualTo("\"path\" \"arg1\" \"arg2\""));
+        src.Source = string.Empty;
+        Assert.That(src.Command, Is.Empty);
     }
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Save
+    ///
+    /// <summary>
+    /// Tests the Save method.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    [Test]
+    public void Save()
+    {
+        var src = new Startup(nameof(StartupTest));
+        Assert.That(src.Enabled,      Is.False);
+        Assert.That(Exists(src.Name), Is.False, "Step1");
+
+        src.Enabled = true;
+        src.Save();
+        Assert.That(Exists(src.Name), Is.True,  "Step2");
+
+        src.Save(true);
+        Assert.That(Exists(src.Name), Is.False, "Step3");
+
+        src.Source = @"path\to\notfound.exe";
+        src.Save(false);
+        Assert.That(Exists(src.Name), Is.True,  "Step4");
+        src.Save(true);
+        Assert.That(Exists(src.Name), Is.False, "Step5");
+
+        src.Save();
+        Assert.That(Exists(src.Name), Is.True,  "Step6");
+        src.Enabled = false;
+        src.Save();
+        Assert.That(Exists(src.Name), Is.False, "Step7");
+    }
+
+    #endregion
+
+    #region Others
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Exists
+    ///
+    /// <summary>
+    /// Determines whether the specified value name exists in the
+    /// registry.
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    private bool Exists(string name) => Registry.GetValue(
+        @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run", name, null
+    ) != null;
+
+    #endregion
 }
