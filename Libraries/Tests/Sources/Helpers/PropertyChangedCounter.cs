8251b1b565f1e57ffc572040e953717ed7e66c63
clown <tt.clown@gmail.com>
2022-11-11 16:35:15 +0900

    change namespace of PropertyChangedCounter class.

--- Libraries/Tests/Sources/Helpers/PropertyChangedCounter.cs
+++ Libraries/Tests/Sources/Helpers/PropertyChangedCounter.cs
@@ -15,7 +15,7 @@
 // limitations under the License.
 //
 /* ------------------------------------------------------------------------- */
-namespace Cube.Tests.Forms;
+namespace Cube.Tests;
 
 using System.ComponentModel;
 using System.Threading;
